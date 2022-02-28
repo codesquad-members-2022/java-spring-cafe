3053347587269ba96a39f390dc367474af1c84f1
Louie <dhdustnr0134@naver.com>
2022-02-28 10:57:37 +0900

    User 클래스 생성

---
+++ src/main/java/com/kakao/cafe/domain/User.java
@@ -0,0 +1,16 @@
+package com.kakao.cafe.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class User {
+
+    private String id;
+    private String password;
+    private String name;
+    private String email;
+
+
+}
