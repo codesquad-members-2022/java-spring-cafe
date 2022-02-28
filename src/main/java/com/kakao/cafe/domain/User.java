ef58c235d05cd3104690dd927e5e34bd5648a9a6
Louie <dhdustnr0134@naver.com>
2022-02-28 11:46:07 +0900

    User 클래스 id 필드 추가, 기존에 있던 id 필드는 userId로 변경

--- src/main/java/com/kakao/cafe/domain/User.java
+++ src/main/java/com/kakao/cafe/domain/User.java
@@ -6,8 +6,8 @@ import lombok.Setter;
 @Getter
 @Setter
 public class User {
-
-    private String id;
+    private Long id;
+    private String userId;
     private String password;
     private String name;
     private String email;
