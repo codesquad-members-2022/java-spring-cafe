faaf01cf8e1ca58fd5892d300e9ff87e5f25d7ac
Minseok-Choi <cms05041@gmail.com>
2022-02-28 22:01:16 +0900

    feat : UserRepository 인터페이스 및 MemoryUserRepository 구현

---
+++ src/main/java/com/kakao/cafe/domain/user/MemoryUserRepository.java
@@ -0,0 +1,27 @@
+package com.kakao.cafe.domain.user;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public class MemoryUserRepository implements UserRepository{
+
+    private static Map<String, User> users = new HashMap<>();
+
+    @Override
+    public void save(User user) {
+        users.put(user.getUserId(), user);
+    }
+
+    @Override
+    public Optional<User> findById(String id) {
+        return Optional.ofNullable(users.get(id));
+    }
+
+    @Override
+    public List<User> findAll() {
+        return users.values().stream().collect(Collectors.toUnmodifiableList());
+    }
+}
