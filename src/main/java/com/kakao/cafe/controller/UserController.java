832cf4a05db847584d6ec3575005bcc8f2296a82
leejohy-0223 <leejohy@naver.com>
2022-02-28 16:22:44 +0900

    feat: html 변경 시 서버 restart 안해도 되도록 devtools 추가

--- src/main/java/com/kakao/cafe/controller/UserController.java
+++ src/main/java/com/kakao/cafe/controller/UserController.java
@@ -2,9 +2,10 @@ package com.kakao.cafe.controller;
 
 import java.util.ArrayList;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.kakao.cafe.domain.users.User;
@@ -14,6 +15,7 @@ import com.kakao.cafe.domain.users.UserRepository;
 public class UserController {
 
     private final UserRepository userRepository = new UserRepository(new ArrayList<>());
+    private Logger logger = LoggerFactory.getLogger(UserController.class);
 
     @GetMapping("/users")
     public void showUsers() {
@@ -21,7 +23,8 @@ public class UserController {
     }
 
     @PostMapping("/users")
-    public String addUser(@ModelAttribute User user) {
+    public String addUser(User user) {
+        logger.info("user name = {}", user.getName());
         userRepository.save(user);
         return "redirect:/users";
     }
