be533b6eadc1ca96c9b30e712543b8f9404a7db5
Kang Changgu (Miller) <rkckr1@naver.com>
2022-02-28 12:46:50 +0900

    feat: 회원가입 기능 - 가입하기 페이지 뷰 구현
    - README.md 에서 회원가입 기능을 구현하기 위한 요구사항을 정리
    - application.properties 내에 동적인 HTML 뷰 관리를 위한 설정
    - /resources/templates 디렉토리 내에 user/form.html 파일 추가
    - UserController 추가 후 메서드에 @GetMapping 어노테이션 작성

---
+++ src/main/java/com/kakao/cafe/UserController.java
@@ -0,0 +1,17 @@
+package com.kakao.cafe;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/users")
+public class UserController {
+
+    @GetMapping
+    public String getUsers(Model model) {
+        return "user/form";
+    }
+
+}
