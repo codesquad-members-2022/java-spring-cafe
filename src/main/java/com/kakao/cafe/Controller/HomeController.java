6cb62387cc452499ac0142b23cb3474df05b2918
WooJin Park <ak2j38@gmail.com>
2022-02-28 17:41:59 +0900

    feature : 메인 페이지 띄우기 추가
    1. thymeleaf 설정을 위해 application.yml에 설정 추가
    2. HomeController를 통해 "/" 주소 index로 매핑
    
    end

---
+++ src/main/java/com/kakao/cafe/Controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.kakao.cafe.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home() {
+        return "index";
+    }
+}
