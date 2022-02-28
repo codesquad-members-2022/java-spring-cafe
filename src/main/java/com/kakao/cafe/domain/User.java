089a879043cfb0990a60a4ede9773355acfe3bf3
who-hoo <who.ho3ov@gmail.com>
2022-02-28 18:31:35 +0900

    :sparkles: 회원 도메인 User 작성

---
+++ src/main/java/com/kakao/cafe/domain/User.java
@@ -0,0 +1,41 @@
+package com.kakao.cafe.domain;
+
+public class User {
+
+    private Long id;
+    private String email;
+    private String nickname;
+    private String password;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
