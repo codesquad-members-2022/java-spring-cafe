7d875ec08f68b798dbee125ec6bc2e39edb1f719
who-hoo <who.ho3ov@gmail.com>
2022-02-28 23:53:01 +0900

    :white_check_mark: MemoryUserRepository 테스트 코드 작성

---
+++ src/test/java/com/kakao/cafe/repository/MemoryUserRepositoryTest.java
@@ -0,0 +1,92 @@
+package com.kakao.cafe.repository;
+
+import static org.assertj.core.api.Assertions.*;
+
+import com.kakao.cafe.domain.User;
+import java.util.Optional;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("MemoryUserRepository 클래스")
+class MemoryUserRepositoryTest {
+
+    MemoryUserRepository repository;
+
+    @BeforeEach
+    void beforeEach() {
+        repository = new MemoryUserRepository();
+        User alreadyPresentUser = new User();
+        alreadyPresentUser.setEmail("already@present.user");
+        alreadyPresentUser.setNickname("already present user1");
+        alreadyPresentUser.setPassword("password1");
+        repository.save(alreadyPresentUser);
+    }
+
+    @Nested
+    @DisplayNameGeneration(value = DisplayNameGenerator.ReplaceUnderscores.class)
+    class save_메소드는 {
+
+        private final User givenUser = new User();
+
+        @BeforeEach
+        void beforeEach() {
+            givenUser.setEmail("new@test.user");
+            givenUser.setNickname("test user1");
+            givenUser.setPassword("password1");
+        }
+
+        @Nested
+        @DisplayNameGeneration(value = DisplayNameGenerator.ReplaceUnderscores.class)
+        class User_객체가_주어지면 {
+
+            @Test
+            @DisplayName("주어진_객체를_저장하고_저장된_객체를_리턴한다")
+            void 주어진_객체를_저장하고_저장된_객체를_리턴한다() {
+                assertThat(givenUser.getId())
+                    .as("저장되지 않은 객체는 아이디가 null 이다")
+                    .isNull();
+                final User saved = repository.save(givenUser);
+                assertThat(saved.getId())
+                    .as("저장된 객체는 아이디가 추가되어 있다.")
+                    .isNotNull();
+            }
+        }
+    }
+
+    @Nested
+    @DisplayNameGeneration(value = DisplayNameGenerator.ReplaceUnderscores.class)
+    class findByEmail_메소드는 {
+
+        private final String presentEmail = "already@present.user";
+        private final String nonPresentEmail = "new@test.user";
+
+        @Nested
+        @DisplayNameGeneration(value = DisplayNameGenerator.ReplaceUnderscores.class)
+        class 존재하는_이메일을_입력_받으면 {
+
+            @Test
+            @DisplayName("해당 이메일을 가진 User 객체를 찾아서 리턴한다")
+            void 해당_이메일을_가진_User_객체를_찾아서_리턴한다() {
+                final Optional<User> result = repository.findByEmail(presentEmail);
+                assertThat(result.isPresent()).isTrue();
+                assertThat(result.get().getEmail()).isEqualTo(presentEmail);
+            }
+        }
+
+        @Nested
+        @DisplayNameGeneration(value = DisplayNameGenerator.ReplaceUnderscores.class)
+        class 존재하지_않는_이메일을_입력_받으면 {
+
+            @Test
+            @DisplayName("Optional.EMPTY를 리턴한다")
+            void Optional_EMPTY를_리턴한다() {
+                final Optional<User> result = repository.findByEmail(nonPresentEmail);
+                assertThat(result.isEmpty()).isTrue();
+            }
+        }
+    }
+}
