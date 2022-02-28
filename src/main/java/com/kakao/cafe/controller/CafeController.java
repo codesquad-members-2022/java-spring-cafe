86679ca841bab1ca1ecb1c8431210a2e08b50ac5
Jaehong Choe <qnalsfj@gmail.com>
2022-03-01 02:21:47 +0900

    feat: 회원가입 form데이터 POST요청 및 요청 후 리다이렉트까지 구현
    - @PostMapping으로 form 데이터가 잘 넘어가고 리다이렉트가 되는지까지만 확인하였음.
    
    - Todo
    회원가입 입력 form으로 넘긴 데이터를 MemoryMemberRepository에 저장한 후 출력하기.

--- src/main/java/com/kakao/cafe/controller/CafeController.java
+++ src/main/java/com/kakao/cafe/controller/CafeController.java
@@ -1,19 +1,41 @@
 package com.kakao.cafe.controller;
 
+import com.kakao.cafe.domain.Member;
+import com.kakao.cafe.service.MemberService;
+import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CafeController {
 
     Logger logger = LoggerFactory.getLogger(CafeController.class);
+    MemberService memberService = new MemberService();
+
+    @PostMapping("/users")
+    public String create(Member form) {
+        Member member = new Member();
+        member.setUserId(form.getUserId());
+        member.setName(form.getName());
+        member.setEmail(form.getEmail());
+        member.setPassword(form.getPassword());
+
+        logger.info("회원가입 호출");
+        System.out.println("form.getUserId = " + form.getUserId());
+        System.out.println("form.getName = " + form.getName());
+        System.out.println("form.getEmail = " + form.getEmail());
+        System.out.println("form.getPassword = " + form.getPassword());
+        memberService.join(member);
+
+        return "redirect:/users";
+    }
 
     @GetMapping("/users")
-    public String helloUser(Model model) {
-        logger.info("GET /users");
+    public String list(Model model) {
         return "users";
     }
 
