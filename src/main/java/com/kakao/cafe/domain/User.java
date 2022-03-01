8f72ef4e5407796424d14240d4190e4904624910
ByeoungJoon Jeon <juni8453@naver.com>
2022-03-01 16:32:05 +0900

    User 도메인 데이터 객체 수정
    1. Getter, Setter 추가

--- src/main/java/com/kakao/cafe/domain/User.java
+++ src/main/java/com/kakao/cafe/domain/User.java
@@ -1,4 +1,42 @@
 package com.kakao.cafe.domain;
 
 public class User {
+
+    private long userIdx;
+    private String userName;
+    private String userPassword;
+    private String userEmail;
+
+    public long getUserIdx() {
+        return userIdx;
+    }
+
+    public void setUserIdx(long userIdx) {
+        this.userIdx = userIdx;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
 }
