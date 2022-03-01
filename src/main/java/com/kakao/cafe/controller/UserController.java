054dc0db2acfc1d97561d37014fad1c95ccd0c66
who-hoo <who.ho3ov@gmail.com>
2022-03-02 01:36:18 +0900

    :sparkles: 회원 컨트롤러(UserController) 생성

---
+++ src/main/java/com/kakao/cafe/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.kakao.cafe.controller;
+
+import com.kakao.cafe.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+}
