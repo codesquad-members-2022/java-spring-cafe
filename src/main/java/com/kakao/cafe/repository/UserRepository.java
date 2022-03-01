16e86bb27b89c2831a700e0edc9476f7923ae14e
jiwoo <zbqmgldjfh@gmail.com>
2022-03-02 08:55:01 +0900

    feat(UserRepository.findById): 사용자의 id를 통해 찾아오는 함수가 Optional을 반환하도록 변경

--- src/main/java/com/kakao/cafe/repository/UserRepository.java
+++ src/main/java/com/kakao/cafe/repository/UserRepository.java
@@ -3,11 +3,12 @@ package com.kakao.cafe.repository;
 import com.kakao.cafe.domain.User;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UserRepository {
     Long save(User user);
 
-    User findById(Long id);
+    Optional<User> findById(Long id);
 
     List<User> findAll();
 
