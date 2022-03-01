2e720478059ebe7329b6ff38215bd61e560ce7b8
WooJin Park <ak2j38@gmail.com>
2022-03-01 15:56:27 +0900

    feature : 회원 도메인 추가
    1. User 클래스 추가
    2. 회원가입 폼에서만 사용하는 속성만 가진 UserCreateDto 추가
    
    end

---
+++ src/main/java/com/kakao/cafe/domain/dto/UserCreateDto.java
@@ -0,0 +1,36 @@
+package com.kakao.cafe.domain.dto;
+
+public class UserCreateDto {
+
+    private String userId;
+    private String password;
+    private String name;
+    private String email;
+
+    public UserCreateDto(String userId, String password, String name, String email) {
+        this.userId = userId;
+        this.password = password;
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
