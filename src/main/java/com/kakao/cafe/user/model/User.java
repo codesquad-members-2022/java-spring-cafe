08c62bf9b055722084ae485627fd28893fb52d24
seyeong <seyoung7555@naver.com>
2022-03-01 14:20:04 +0900

    feat(User): 회원 모델에 id 값 추가
    - 회원의 고유번호인 id 필드를 생성했습니다.

---
+++ src/main/java/com/kakao/cafe/user/model/User.java
@@ -0,0 +1,50 @@
+package com.kakao.cafe.user.model;
+
+public class User {
+    private Long id;
+    private String userId;
+    private String password;
+    private String name;
+    private String email;
+
+    public User(String userId, String password, String name, String email) {
+        this.userId = userId;
+        this.password = password;
+        this.name = name;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId='" + userId + '\'' +
+                ", password='" + password + '\'' +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
