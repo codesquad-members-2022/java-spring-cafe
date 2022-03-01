dc76a15de0f86cb9b3756230e42c14f19d5b4158
jiwoo <zbqmgldjfh@gmail.com>
2022-03-01 11:53:41 +0900

    feat(UserController.addForm): 등록 Form을 보여주고 저장하기 구현 완료
    사용자가 "/users/create" 로 get 요청을 하면 addForm()으로 폼을 보여준다.
    
    사용자가 폼에 값을 입력하여 "/users/create" 로 post 요청을 하면 저장후, list로 redirect 해 준다.

---
+++ src/main/java/com/kakao/cafe/web/users/UserController.java
@@ -0,0 +1,47 @@
+package com.kakao.cafe.web.users;
+
+import com.kakao.cafe.domain.User;
+import com.kakao.cafe.repository.MemoryUserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/users")
+public class UserController {
+
+    private final MemoryUserRepository repository;
+    private final Logger log = LoggerFactory.getLogger(UserController.class);
+
+    public UserController(MemoryUserRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping
+    public String users(Model model){
+        List<User> members = repository.findAll();
+        log.info("users list = {}", members);
+        model.addAttribute("members", members);
+        return "/user/list";
+    }
+
+    @GetMapping("/create")
+    public String addForm(){
+        log.info("create form 접근");
+        return "/user/form";
+    }
+
+    @PostMapping("/create")
+    public String saveUser(@ModelAttribute User user){
+        repository.save(user);
+        log.info("save user = {}", user);
+        return "redirect:/users";
+    }
+}
