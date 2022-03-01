49bb3399065a070bac28c93b6d47331719936d79
jiwoo <zbqmgldjfh@gmail.com>
2022-03-01 15:20:28 +0900

    feat(UserController.getUserProfile): 사용자의 profile 정보를 보여주는 메서드 작성

--- src/main/java/com/kakao/cafe/web/users/UserController.java
+++ src/main/java/com/kakao/cafe/web/users/UserController.java
@@ -6,10 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,9 +23,9 @@ public class UserController {
 
     @GetMapping
     public String users(Model model) {
-        List<User> members = repository.findAll();
-        log.info("users list = {}", members);
-        model.addAttribute("members", members);
+        List<User> users = repository.findAll();
+        log.info("users list = {}", users);
+        model.addAttribute("users", users);
         return "/user/list";
     }
 
@@ -43,5 +40,13 @@ public class UserController {
         repository.save(user);
         log.info("save user = {}", user);
         return "redirect:/users";
+    }
+
+    @GetMapping("/{userId}")
+    public String getUserProfile(@PathVariable Long userId, Model model) {
+        User findUser = repository.findById(userId);
+        log.info("user profile = {}", findUser);
+        model.addAttribute("user", findUser);
+        return "/user/profile";
     }
 }
