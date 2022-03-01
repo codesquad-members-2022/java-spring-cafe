ae4f0fa18e02203859bf626fb9b6668ffc416790
ikjo93 <auddlr100@naver.com>
2022-03-01 18:34:49 +0900

    feat : UserController 클래스에 사용자 URL 요청 시 로깅(Logging) 기능 추가

--- src/main/java/com/kakao/cafe/controller/UserController.java
+++ src/main/java/com/kakao/cafe/controller/UserController.java
@@ -2,6 +2,8 @@ package com.kakao.cafe.controller;
 
 import com.kakao.cafe.domain.UserInformation;
 import com.kakao.cafe.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -19,21 +21,27 @@ public class UserController {
         this.userService = userService;
     }
 
+    Logger logger = LoggerFactory.getLogger(UserController.class);
+
     @GetMapping("/users")
     public String getUserList(Model model) {
+        logger.info("GET /users");
         model.addAttribute("users", userService.findAllUsers());
         return "/user/list";
     }
 
     @GetMapping("/users/{userId}")
     public String getUserProfile(@PathVariable String userId, Model model) {
+        logger.info("GET /users/{}", userId);
         model.addAttribute("user", userService.findOneUser(userId).get());
         return "/user/profile";
     }
 
     @PostMapping("/users")
     public String createUserInformation(UserInformation userInformation) {
-
+        logger.info("POST /users userId = {} password = {} name = {} email = {}"
+                , userInformation.getUserId(), userInformation.getPassword()
+                , userInformation.getName(), userInformation.getEmail());
         userService.join(userInformation);
 
         return "redirect:/users";
