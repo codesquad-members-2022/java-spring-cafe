cd0285afb82536ef0276e08f353ec943e19cd969
ByeoungJoon Jeon <juni8453@naver.com>
2022-03-01 16:32:45 +0900

    UserRepository 인터페이스와 이를 상속하는 JdbcTemplateUserRepository 클래스를 생성했습니다.
    1. UserRepository
    가. void createUser(User user);
    회원 객체를 받아와 새로운 회원을 생성합니다.
    
    나. List <User> findAllUsers();
    회원 테이블을 모두 가져와 List 타입으로 받아냅니다.
    
    다. Optional<User> findUser(String userName);
    회원 아이디를 받아와 해당 아이디를 찾아 Optional<User> 타입으로 받아냅니다.
    
    2. JdbcTemplateUserRepository
    DataSource 를 외부에서 주입받아 JdbcTemplate 을 생성합니다.
    create()   - INSERT 구문이라 따로 return 하는 타입이 없습니다.
    finduser() - SQL 문에 WHERE userName = ? 을 추가해서 userName 에 맞는 회원을 찾아냅니다.
    findUser() - 회원 테이블을 모두 조회하고 결과를 return 합니다.

--- src/main/java/com/kakao/cafe/repository/UserRepository.java
+++ src/main/java/com/kakao/cafe/repository/UserRepository.java
@@ -1,2 +1,15 @@
-package com.kakao.cafe.repository;public interface UserRepository {
+package com.kakao.cafe.repository;
+
+import com.kakao.cafe.domain.User;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserRepository {
+
+    void createUser(User user);
+
+    List<User> findAllUsers();
+
+    Optional<User> findUser(String userName);
 }
