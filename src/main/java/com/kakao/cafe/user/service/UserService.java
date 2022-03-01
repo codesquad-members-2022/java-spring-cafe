ccdbffeacae7b56166afedde6fb01977f965d06e
seyeong <seyoung7555@naver.com>
2022-03-01 15:20:27 +0900

    feat(UserService): 회원 프로필 조회 기능 추가
    - userId를 통해 유저를 조회하는 로직을 수행하는 findByUserId()를 구현했습니다.
    - 만약 해당 userId의 유저가 존재하지 않을 경우 Exception이 발생합니다.

--- src/main/java/com/kakao/cafe/user/service/UserService.java
+++ src/main/java/com/kakao/cafe/user/service/UserService.java
@@ -21,4 +21,9 @@ public class UserService {
     public List<User> findUsers() {
         return userRepository.findAll();
     }
+
+    public User findByUserId(String userId) {
+        return userRepository.findByUserId(userId)
+                .orElseThrow(() -> {throw new IllegalArgumentException("해당 회원이 존재하지 않습니다.");});
+    }
 }
