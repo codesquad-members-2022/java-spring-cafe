8689eb1516cad6bcf833a82e306e38bdfd5b922f
seyeong <seyoung7555@naver.com>
2022-03-01 14:24:35 +0900

    feat(UserService): 회원과 관련된 비즈니스 로직을 처리할 UserService 객체 생성
    - 회원과 관련된 Service를 담당합니다.

---
+++ src/main/java/com/kakao/cafe/user/service/UserService.java
@@ -0,0 +1,24 @@
+package com.kakao.cafe.user.service;
+
+import com.kakao.cafe.user.model.User;
+import com.kakao.cafe.user.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public void join(User user) {
+        userRepository.save(user);
+    }
+
+    public List<User> findUsers() {
+        return userRepository.findAll();
+    }
+}
