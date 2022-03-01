90eab3aedd8483553c6164ccb8f26d13a10b4bf8
seyeong <seyoung7555@naver.com>
2022-03-01 22:56:56 +0900

    refactor(UserService): 예외 메시지 상수로 분리

--- src/main/java/com/kakao/cafe/service/UserService.java
+++ src/main/java/com/kakao/cafe/service/UserService.java
@@ -9,6 +9,8 @@ import java.util.List;
 
 @Service
 public class UserService {
+    private static final String NOT_FOUND_USER_MESSAGE = "해당 회원이 존재하지 않습니다.";
+    private static final String DUPLICATED_USER_MESSAGE = "이미 존재하는 회원입니다.";
     private final UserRepository userRepository;
 
     public UserService(UserRepository userRepository) {
@@ -27,12 +29,12 @@ public class UserService {
 
     public User findByUserId(String userId) {
         return userRepository.findByUserId(userId)
-                .orElseThrow(() -> new IllegalArgumentException("해당 회원이 존재하지 않습니다."));
+                .orElseThrow(() -> new IllegalArgumentException(NOT_FOUND_USER_MESSAGE));
     }
 
     private void validateDuplicatedUserId(User user) {
         userRepository.findByUserId(user.getUserId()).ifPresent(m -> {
-            throw new IllegalStateException("이미 존재하는 회원입니다.");
+            throw new IllegalStateException(DUPLICATED_USER_MESSAGE);
         });
     }
 }
