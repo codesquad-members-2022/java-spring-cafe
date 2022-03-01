5dbf14dbc141006eea6e16fbf9e286f4e41cc708
who-hoo <who.ho3ov@gmail.com>
2022-03-02 01:40:26 +0900

    :sparkles: 회원 등록, 회원 조회 기능 구현
    - 회원 등록(signUp) 기능 구현, 회원 등록 폼 화면에서 데이터를 전달받을 객체로 SignUpRequestDto 새로 작성
    - 회원 조회(list) 기능 구현, 도메인 Model인 User 대신 응답 객체로 UserDto 새로 작성하여 사용함(노출되어서는 안 되는 정보를 감추기 위해서)

--- src/main/java/com/kakao/cafe/controller/UserController.java
+++ src/main/java/com/kakao/cafe/controller/UserController.java
@@ -1,8 +1,16 @@
 package com.kakao.cafe.controller;
 
+import com.kakao.cafe.controller.dto.UserDto;
+import com.kakao.cafe.domain.User;
+import com.kakao.cafe.controller.dto.SignUpRequestDto;
 import com.kakao.cafe.service.UserService;
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
@@ -12,5 +20,26 @@ public class UserController {
     @Autowired
     public UserController(UserService userService) {
         this.userService = userService;
+    }
+
+    @PostMapping("/users")
+    public String signUp(SignUpRequestDto form) {
+        User user = new User();
+        user.setEmail(form.getEmail());
+        user.setNickname(form.getNickname());
+        user.setPassword(form.getPassword());
+        userService.signUp(user);
+        return "redirect:/users";
+    }
+
+    @GetMapping("/users")
+    public String list(Model model) {
+        List<UserDto> users = new ArrayList<>();
+        for (User user : userService.findUsers()) {
+            users.add(UserDto.from(user));
+        }
+        model.addAttribute("users", users);
+        model.addAttribute("totalUserCount", users.size());
+        return "user/list";
     }
 }
