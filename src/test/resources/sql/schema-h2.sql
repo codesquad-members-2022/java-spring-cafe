95287b74c8e8facc9530156d7489513b49d2db48
jinan159 <wlsdhks0423@naver.com>
2022-03-01 22:38:37 +0900

    build: cafe_users 테이블 수정
    - userId 컬럼 크기 varchar(10) -> (20)
    - name 컬럼 추가

--- src/test/resources/sql/schema-h2.sql
+++ src/test/resources/sql/schema-h2.sql
@@ -1,8 +1,9 @@
 CREATE TABLE `cafe_users` (
     id BIGINT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    email VARCHAR(320) NOT NULL,
-    userId VARCHAR(10) NOT NULL,
+    userId VARCHAR(20) NOT NULL UNIQUE,
     passwd CHAR(41) NOT NULL,
+    name VARCHAR(40),
+    email VARCHAR(320),
     created_date DATETIME NOT NULL,
     modified_date DATETIME NOT NULL
 );
