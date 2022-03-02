34781cb526c1ce59eaf92e257e69c35e651da634
jiwoo <zbqmgldjfh@gmail.com>
2022-03-02 13:52:56 +0900

    feat(ArticleRepository): ArticleRepository Interface 작성하기
    기본 CRUD에 해당되는 Interface 명세 목록

---
+++ src/main/java/com/kakao/cafe/domain/Article.java
@@ -0,0 +1,46 @@
+package com.kakao.cafe.domain;
+
+public class Article {
+    private Long id;
+    private String userId;
+    private String title;
+    private String content;
+
+    public Article(String userId, String title, String content) {
+        this.userId = userId;
+        this.title = title;
+        this.content = content;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
