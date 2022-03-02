1cd1e8e46e917961414da857e298cd5e4a6ce879
Louie <dhdustnr0134@naver.com>
2022-03-02 09:35:39 +0900

    등록했던 ViewResolver 삭제

--- src/main/java/com/kakao/cafe/config/MvcConfig.java
+++ src/main/java/com/kakao/cafe/config/MvcConfig.java
@@ -1,25 +1,17 @@
 package com.kakao.cafe.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.ViewResolver;
+import org.springframework.core.Ordered;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
 
-    @Bean
-    public ViewResolver viewResolver(){
-        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
-        viewResolver.setPrefix("/templates/");
-        viewResolver.setSuffix(".html");
-        return viewResolver;
-    }
-
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
+        registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
+
         registry.addViewController("/users/login").setViewName("user/login");
         registry.addViewController("/users/form").setViewName("user/form");
     }
