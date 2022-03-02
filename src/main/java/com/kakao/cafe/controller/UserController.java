d5704960f3fb30d6ddbc4c1380fb4966169f66f3
geombong <shoy1415@gmail.com>
2022-03-02 14:02:08 +0900

    feat. UserController 클래스 메서드 추가

--- src/main/java/com/kakao/cafe/controller/UserController.java
+++ src/main/java/com/kakao/cafe/controller/UserController.java
@@ -1,20 +1,29 @@
 package com.kakao.cafe.controller;
 
 import com.kakao.cafe.entity.User;
+import com.kakao.cafe.service.UserService;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/users")
 public class UserController {
 
+    private final UserService userService;
     private final Logger log = LoggerFactory.getLogger(UserController.class);
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
     @GetMapping("/signup")
     public String signup() {
@@ -22,9 +31,18 @@ public class UserController {
         return "user/form";
     }
 
+    @GetMapping("/list")
+    public String findAllUser(Model model) {
+        List<User> users = userService.findUsers();
+        model.addAttribute("users", users);
+        log.info("유저 리스트 페이지 접속");
+        return "user/list";
+    }
+
     @PostMapping("/create")
     public String users(@ModelAttribute User user) {
+        userService.signUp(user);
         log.info("signUp user = {}", user);
-        return "redirect:/list";
+        return "redirect:list";
     }
 }
