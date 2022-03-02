dd2c96faaf3d1689ac3151ed7e61f9bf555de28e
geombong <shoy1415@gmail.com>
2022-03-02 17:37:46 +0900

    feat. 유저 프로필 검색을 위한 GET Mapping 추가

--- src/main/java/com/kakao/cafe/controller/UserController.java
+++ src/main/java/com/kakao/cafe/controller/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -44,5 +45,14 @@ public class UserController {
         userService.signUp(user);
         log.info("signUp user = {}", user);
         return "redirect:list";
+    }
+
+    @GetMapping("/{userId}")
+    public String profile(@PathVariable String userId, Model model) {
+        System.out.println(userId);
+        User findUser = userService.findIdUser(userId);
+        log.info("findUser = {}", findUser);
+        model.addAttribute("user", findUser);
+        return "user/profile";
     }
 }
