4cf369521ddd33bc5617dd127b055a4f4440e3e2
ByeoungJoon Jeon <juni8453@naver.com>
2022-03-02 21:57:16 +0900

    UserService 테스트를 위한 UserServiceTest 클래스 생성
    1. beforEach()
    각 테스트 메서드 실행 전 userService, userRepository 를 생성하도록 했습니다.
    userService 는 userRepository 에 의존적이기 때문에 userService 객체를 생성할 때
    userRepository 객체를 생성해 주입하도록 했습니다.
    유저의 저장소인 store 을 getUserStore() 통해 가져올 수 있도록 했습니다.
    유저의 Idx 를 셋팅하기 위해 long 타입의 userCount 변수를 생성합니다.
    
    2. AfterEach()
    테스트가 하나 종료될 때 마다 유저 저장소가 비어져있어야하기 때문에 clear() 메서드를 활용했습니다.
    
    3. 유저_생성_테스트()
    유저를 셋팅하고 Service 의 createUser() 메서드를 테스트 합니다.
    셋팅한 유저의 userName 과 저장소에 저장된 유저를 찾아 찾은 유저의 userName 과 같은지 테스트합니다.
    
    3. 중복_유저_예외발생_테스트()
    같은 userName 을 가진 유저 객체를 2개 만들고 첫 번째 유저를 먼저 create() 합니다.
    이후 두 번째 유저를 create() 할 때 예외가 나는지 체크합니다.
    
    4. 유저_찾기_테스트()
    유저 객체를 만들고 저장소에 put() 합니다.
    Service 의 findUser() 메서드를 통해 저장소에 있는 유저 객체를 foundUser 변수에 담습니다.
    이후 저장소에서 찾은 유저의 userName 과 처음 생성한 user의 userName 이 같은지 테스트합니다.
    
    5. 모든_유저_찾기_테스트()
    유저 객체를 2개 만들고 저장소에 put() 합니다.
    Service 의 findAllUsers() 메서드를 통해 저장소에 있는 모든 유저 객체를 List 타입의 allUsers 변수에 담습니다.
    이후 저장소에서 찾은 모든 유저의 userName, userIdx 와 처음 생성한 user 의 userName, userIdx 가 같은지 테스트합니다.

---
+++ src/test/java/com/kakao/cafe/service/UserServiceTest.java
@@ -0,0 +1,107 @@
+package com.kakao.cafe.service;
+
+import com.kakao.cafe.domain.User;
+import com.kakao.cafe.repository.MemoryUserRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceTest {
+
+    UserService userService;
+    MemoryUserRepository repository;
+    Map<Long, User> store;
+    long userCount = 0;
+
+    // 각 테스트 전
+    @BeforeEach
+    void beforeEach() {
+        repository = new MemoryUserRepository();
+        userService = new UserService(repository);
+        store = repository.getUserStore();
+    }
+
+    // 각 테스트 후
+    @AfterEach
+    void afterEach() {
+        repository.clear();
+    }
+
+    @Test
+    @DisplayName("유저의 userName 과 createUser 이후 찾아낸 유저의 userName 이 동일해야 한다.")
+    void 유저_생성_테스트() {
+        // given
+        User user = new User();
+        user.setUserName("testName");
+
+        // when
+        userService.createUser(user);
+        User foundUser = repository.findUser(user.getUserName()).get();
+
+        // then
+        assertThat(user.getUserName()).isEqualTo(foundUser.getUserName());
+
+    }
+
+    @Test
+    @DisplayName("userName 이 같은 유저가 생성되면 예외가 발생해야 한다.")
+    void 중복_유저_예외발생_테스트() {
+        User user1 = new User();
+        user1.setUserName("Test");
+
+        User user2 = new User();
+        user2.setUserName("Test");
+
+        userService.createUser(user1);
+
+        assertThrows(IllegalStateException.class, () -> userService.createUser(user2));
+    }
+
+    @Test
+    @DisplayName("유저가 생성되면 생성된 유저의 userName 과 찾아낸 userName 이 동일해야 한다.")
+    void 유저_찾기_테스트() {
+        userCount = 0;
+
+        User user = new User();
+        user.setUserName("Test");
+        user.setUserIdx(++userCount);
+
+        store.put(user.getUserIdx(), user);
+
+        User foundUser = userService.findUser(user.getUserName()).get();
+
+        assertThat(foundUser.getUserName()).isEqualTo(user.getUserName());
+    }
+
+    @Test
+    @DisplayName("2개 이상의 유저가 생성되면 생성된 만큼의 유저의 userName 과 찾아낸 userName 이 동일해야 한다.")
+    void 모든_유저_찾기_테스트() {
+        userCount = 0;
+
+        User user1 = new User();
+        User user2 = new User();
+        user1.setUserName("testUser1");
+        user1.setUserIdx(++userCount);
+
+        user2.setUserName("testUser2");
+        user2.setUserIdx(++userCount);
+
+        store.put(user1.getUserIdx(), user1);
+        store.put(user2.getUserIdx(), user2);
+
+        List<User> allUsers = userService.findAllUsers();
+        User foundUser1 = allUsers.get(0);
+        User foundUser2 = allUsers.get(1);
+
+        assertThat(foundUser1.getUserName()).isEqualTo(user1.getUserName());
+        assertThat(foundUser1.getUserIdx()).isEqualTo(user1.getUserIdx());
+        assertThat(foundUser2.getUserName()).isEqualTo(user2.getUserName());
+        assertThat(foundUser2.getUserIdx()).isEqualTo(user2.getUserIdx());
+    }
+}
