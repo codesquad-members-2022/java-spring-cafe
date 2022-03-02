f6d725c6a493e2b09481036208ebd80a805ef506
ikjo93 <auddlr100@naver.com>
2022-03-02 16:34:22 +0900

    refactor : HTML 중복 제거 작업
    작업내용
    1. mustache의 partial 기능을 이용하여 index.html, /user/list.html, /user/form.html, /qna/form.html 코드 상  header, navigation bar, footer 부분들의 중복을 제거하였습니다.
    2. 기존 static 폴더에 있던 html 파일들이 templates 폴더로 오면서 css 기능이 적용되지 않는 부분이 생겨 기존 MvcConfig에서 addResourceHandelrs 메서드를 통해 css 기능이 적용되도록 했습니다.

--- src/main/java/com/kakao/cafe/config/MvcConfig.java
+++ src/main/java/com/kakao/cafe/config/MvcConfig.java
@@ -2,6 +2,7 @@ package com.kakao.cafe.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -15,5 +16,10 @@ public class MvcConfig implements WebMvcConfigurer {
         registry.addViewController("/users/form").setViewName("user/form");
         registry.addViewController("/users/login").setViewName("user/login");
         registry.addViewController("/questions/form").setViewName("qna/form");
+    }
+
+    @Override
+    public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("static/**").addResourceLocations("classpath:/static/").setCachePeriod(20);
     }
 }
