f55ff548d70fc1aa0b9f3e42cc75cca352b21328
geombong <shoy1415@gmail.com>
2022-03-02 12:10:41 +0900

    feat. User 클래스 toString 생성

--- src/main/java/com/kakao/cafe/entity/User.java
+++ src/main/java/com/kakao/cafe/entity/User.java
@@ -11,31 +11,35 @@ public class User {
     private final LocalDate userRegistrationDate;
     private Long id;
 
-    public User(String email, String nickname, String name, String password) {
+    public User(String email, String userId, String name, String password) {
         this.email = email;
-        this.userId = nickname;
+        this.userId = userId;
         this.name = name;
         this.password = password;
         this.userRegistrationDate = LocalDate.now();
+    }
+
+    public String getName() {
+        return name;
     }
 
     public Long getId() {
         return id;
     }
 
-    public void setId(long id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
-    public String getEmail() {
-        return this.email;
-    }
-
-    public String getUserId() {
-        return this.userId;
-    }
-
-    public LocalDate getUserRegistrationDate() {
-        return this.userRegistrationDate;
+    @Override
+    public String toString() {
+        return "User{" +
+                "email='" + email + '\'' +
+                ", userId='" + userId + '\'' +
+                ", name='" + name + '\'' +
+                ", password='" + password + '\'' +
+                ", userRegistrationDate=" + userRegistrationDate +
+                ", id=" + id +
+                '}';
     }
 }
