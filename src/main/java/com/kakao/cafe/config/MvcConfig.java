781a8a2a5a67dd950a8f4ca8f49a65f801ab14e3
Kang Changgu (Miller) <rkckr1@naver.com>
2022-03-02 12:21:50 +0900

    docs: MvcConfig 내 참고한 주석 추가

--- src/main/java/com/kakao/cafe/config/MvcConfig.java
+++ src/main/java/com/kakao/cafe/config/MvcConfig.java
@@ -9,11 +9,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupp
 @Configuration
 public class MvcConfig extends WebMvcConfigurationSupport {
 
+    // https://imbf.github.io/spring/2020/05/03/Spring-HiddenHttpMethodFilter.html
     @Bean
     public HiddenHttpMethodFilter httpMethodFilter() {
         return new HiddenHttpMethodFilter();
     }
 
+    // https://stackoverflow.com/questions/24661289/spring-boot-not-serving-static-content
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/**")
