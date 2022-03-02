2215b0b4ee9541c3fdb1b2eb71881af94de390c9
seyeong <seyoung7555@naver.com>
2022-03-02 16:48:35 +0900

    feat(ArticleController): 게시글 조회/등록을 위한 Controller 작성

---
+++ src/main/java/com/kakao/cafe/controller/ArticleController.java
@@ -0,0 +1,38 @@
+package com.kakao.cafe.controller;
+
+import com.kakao.cafe.controller.dto.ArticleSaveRequestDto;
+import com.kakao.cafe.domain.posts.Article;
+import com.kakao.cafe.service.ArticleService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class ArticleController {
+
+    private final ArticleService articleService;
+
+    public ArticleController(ArticleService articleService) {
+        this.articleService = articleService;
+    }
+
+    @GetMapping("/qna")
+    public String articleList() {
+        return "qna/form";
+    }
+
+    @PostMapping("/questions")
+    public String save(ArticleSaveRequestDto requestDto) {
+        articleService.save(requestDto);
+        return "redirect:/";
+    }
+
+    @GetMapping("/articles/{id}")
+    public String articleDetail(@PathVariable Long id, Model model) {
+        Article article = articleService.findById(id);
+        model.addAttribute("article", article);
+        return "qna/show";
+    }
+}
