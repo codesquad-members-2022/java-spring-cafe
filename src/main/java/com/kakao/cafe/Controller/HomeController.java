2adaa3dfe727a2f5ccb6838155f014151881a0f8
WooJin Park <ak2j38@gmail.com>
2022-03-03 01:44:21 +0900

    feature : 컨트롤러 수정
    1. 인덱스 이동 GetMapping으로 수정
    2. 회원 목록, 회원 조회 기능 추가
    3. 회원가입 페이지 이동 기능 추가
    
    end

--- src/main/java/com/kakao/cafe/Controller/HomeController.java
+++ src/main/java/com/kakao/cafe/Controller/HomeController.java
@@ -1,12 +1,12 @@
 package com.kakao.cafe.Controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
-    @RequestMapping("/")
+    @GetMapping("/")
     public String home() {
         return "index";
     }
--- src/main/java/com/kakao/cafe/Controller/UserController.java
+++ src/main/java/com/kakao/cafe/Controller/UserController.java
@@ -2,9 +2,16 @@ package com.kakao.cafe.Controller;
 
 import com.kakao.cafe.domain.User;
 import com.kakao.cafe.domain.dto.UserCreateDto;
+import com.kakao.cafe.domain.dto.UserListDto;
 import com.kakao.cafe.service.UserService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 public class UserController {
@@ -15,12 +22,50 @@ public class UserController {
         this.userService = userService;
     }
 
+    @GetMapping("/user/signup")
+    public String signup(Model model) {
+        model.addAttribute("userCreateDto", new UserCreateDto());
+
+        return "user/form";
+    }
+
+
     @PostMapping("/users")
     public String create(UserCreateDto userCreateDto) {
+        System.out.println("userCreateDto.toString() = " + userCreateDto.toString());
+        
         User user = new User(userService.nextUserSequence(), userCreateDto);
 
-        User joinUser = userService.join(user);
+        userService.save(user);
 
         return "redirect:/users";
+    }
+
+    @GetMapping("/users")
+    public String list(Model model) {
+        for (User user : userService.findUsers()) {
+            System.out.println("user.toString() = " + user.toString());
+        }
+        
+        List<UserListDto> allUsers = userService.findUsers().stream()
+                .map(user -> new UserListDto(user))
+                .collect(Collectors.toList());
+
+        for (UserListDto allUser : allUsers) {
+            System.out.println(allUser.getUserId() + " " + allUser.getName() + " " + allUser.getEmail());
+        }
+
+        model.addAttribute("allUsers", allUsers);
+
+        return "/user/list";
+    }
+
+    @GetMapping("users/{userId}")
+    public String profile(@PathVariable String userId, Model model) {
+        User findUser = userService.findByUserId(userId);
+
+        model.addAttribute("findUser", findUser);
+
+        return "/user/profile";
     }
 }
