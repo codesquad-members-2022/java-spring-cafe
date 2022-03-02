35f80bd45c831c783ed9a7e46cb0a7e5f3533d44
ByeoungJoon Jeon <juni8453@naver.com>
2022-03-02 14:41:41 +0900

    MemoryUserRepository 클래스 생성
    기존에 DB 와 연결한 클래스를 지우고 Map을 사용해 User 를 저장하는 repository 클래스를
    
    새로 생성했습니다.
    
    DB에서 Auto_increment을 이용해 자동으로 Idx를 늘려주는 기능은 ++ 연산자를 이용해 구현했습니다.

--- src/main/java/com/kakao/cafe/repository/MemoryUserRepository.java
+++ src/main/java/com/kakao/cafe/repository/MemoryUserRepository.java
@@ -1,2 +1,31 @@
-package com.kakao.cafe.repository;public class MemoryUserRepository {
+package com.kakao.cafe.repository;
+
+import com.kakao.cafe.domain.User;
+import org.springframework.stereotype.Repository;
+
+import java.util.*;
+
+@Repository
+public class MemoryUserRepository implements UserRepository {
+
+    private final Map<Long, User> userStore = new HashMap<>();
+    private static long userIdx = 0;
+
+    @Override
+    public void createUser(User user) {
+        user.setUserIdx(++userIdx);
+        userStore.put(user.getUserIdx(), user);
+    }
+
+    @Override
+    public List<User> findAllUsers() {
+        return new ArrayList<>(userStore.values());
+    }
+
+    @Override
+    public Optional<User> findUser(String userName) {
+        return userStore.values().stream()
+                .filter(user -> user.getUserName().equals(userName))
+                .findAny();
+    }
 }
