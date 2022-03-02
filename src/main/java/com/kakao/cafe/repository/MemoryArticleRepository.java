d7095111cbaac3054619789a3e1612e4c25d7141
jiwoo <zbqmgldjfh@gmail.com>
2022-03-02 14:35:35 +0900

    feat(MemoryArticleRepository): Article 의 Interface 의 구현체인 MemoryArticleRepository 작성 완료

---
+++ src/main/java/com/kakao/cafe/repository/MemoryArticleRepository.java
@@ -0,0 +1,53 @@
+package com.kakao.cafe.repository;
+
+import com.kakao.cafe.domain.Article;
+import com.kakao.cafe.domain.User;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class MemoryArticleRepository implements ArticleRepository {
+    private final Map<Long, Article> store = new ConcurrentHashMap<>();
+    private Long sequence = 0L;
+
+    @Override
+    public Long save(Article article) {
+        article.setId(++sequence);
+        store.put(article.getId(), article);
+        return article.getId();
+    }
+
+    @Override
+    public Optional<Article> findById(Long id) {
+        return store.keySet().stream()
+                .filter(sequence -> sequence.equals(id))
+                .map(store::get)
+                .findAny();
+    }
+
+    @Override
+    public List<Article> findAll() {
+        return new ArrayList<>(store.values());
+    }
+
+    @Override
+    public boolean delete(Long id) {
+        if(store.containsKey(id)){
+            store.remove(id);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void update(Long id, Article article) {
+        if(findById(id).isPresent()){
+            Article find = findById(id).get();
+            find.setTitle(article.getTitle());
+            find.setContent(article.getContent());
+        }
+    }
+}
