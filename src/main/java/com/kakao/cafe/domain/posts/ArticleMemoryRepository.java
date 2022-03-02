f90c33c39ed75a633591d17142f78f34af5454e9
seyeong <seyoung7555@naver.com>
2022-03-02 16:51:25 +0900

    feat(ArticleMemoryRepository): List로 Article을 저장하는 Repository 구현체 생성

---
+++ src/main/java/com/kakao/cafe/domain/posts/ArticleMemoryRepository.java
@@ -0,0 +1,31 @@
+package com.kakao.cafe.domain.posts;
+
+import org.springframework.stereotype.Repository;
+
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+@Repository
+public class ArticleMemoryRepository implements ArticleRepository {
+    private final List<Article> articleList = new ArrayList<>();
+    private final SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+
+    @Override
+    public void save(Article article) {
+        article.setId((long) (articleList.size() + 1));
+        article.setCreatedDate(formatter.format(new Date()));
+        articleList.add(article);
+    }
+
+    @Override
+    public List<Article> findAllDesc() {
+        List<Article> descList = articleList;
+        Collections.reverse(descList);
+        return descList;
+    }
+
+    @Override
+    public Optional<Article> findById(Long id) {
+        return articleList.stream().filter(m -> m.getId().equals(id)).findFirst();
+    }
+}
