0a4ac814d5c271381397e1a0212ab4c1faf85e38
seyeong <seyoung7555@naver.com>
2022-03-02 16:49:52 +0900

    feat(ArticleSaveRequestDto): 게시글 저장 요청을 위한 Dto 객체 생성

---
+++ src/main/java/com/kakao/cafe/controller/dto/ArticleSaveRequestDto.java
@@ -0,0 +1,19 @@
+package com.kakao.cafe.controller.dto;
+
+import com.kakao.cafe.domain.posts.Article;
+
+public class ArticleSaveRequestDto {
+    private String writer;
+    private String title;
+    private String contents;
+
+    public ArticleSaveRequestDto(String writer, String title, String contents) {
+        this.writer = writer;
+        this.title = title;
+        this.contents = contents;
+    }
+
+    public Article toEntity() {
+        return new Article(writer, title, contents);
+    }
+}
