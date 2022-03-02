14f59368b2858b4f7ddf13d2ba5fd671e45e0415
Jaehong Choe <qnalsfj@gmail.com>
2022-03-03 00:21:46 +0900

    refactor: 컨트롤러에 @RequestMapping 추가
    - 매핑된 메서드마다 users를 반복적으로 적지 않아도 되도록 클래스 레벨에서 RequestMapping
    등록.

--- src/main/java/com/kakao/cafe/controller/CafeController.java
+++ src/main/java/com/kakao/cafe/controller/CafeController.java
@@ -3,20 +3,25 @@ package com.kakao.cafe.controller;
 import com.kakao.cafe.domain.User;
 import com.kakao.cafe.service.UserService;
 import java.util.List;
+import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+@RequestMapping("users")
 public class CafeController {
 
     Logger logger = LoggerFactory.getLogger(CafeController.class);
     UserService userService = new UserService();
 
-    @PostMapping("/users")
+    @PostMapping
     public String create(User form) {
         User user = new User();
         user.setUserId(form.getUserId());
@@ -29,19 +34,19 @@ public class CafeController {
         return "redirect:/users";
     }
 
-    @GetMapping("/users")
+    @GetMapping
     public String list(Model model) {
         List<User> users = userService.findUsers();
         model.addAttribute("users", users);
-        System.out.println(users);
-        return "/users/list";
-    }
-
-    @GetMapping("/test")
-    public String list2(Model model) {
-        List<User> users = userService.findUsers();
-        model.addAttribute("users", users);
-        System.out.println(users);
         return "users/list";
     }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public String userDetails(@PathVariable("id") Long id, Model model) {
+        Optional<User> user = userService.findOne(id);
+        model.addAttribute("user", user.get());
+
+        return "users/profile";
+    }
+
 }
