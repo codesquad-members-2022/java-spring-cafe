85e986ac7efee23523f99e63b1586094897b0bf7
hsjang <hsjang0405@gmail.com>
2022-03-03 15:51:40 +0900

    ✨  게시글 상세보기 구현 완료

--- src/main/java/com/kakao/cafe/web/controller/ArticleController.java
+++ src/main/java/com/kakao/cafe/web/controller/ArticleController.java
@@ -1,11 +1,14 @@
 package com.kakao.cafe.web.controller;
 
 import com.kakao.cafe.web.domain.article.Article;
+import com.kakao.cafe.web.domain.user.User;
+import com.kakao.cafe.web.domain.user.UserDto;
 import com.kakao.cafe.web.service.ArticleService;
 import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -35,6 +38,14 @@ public class ArticleController {
 		List<Article> articles = articleService.findAllArticle();
 		model.addAttribute("articles", articles);
 		return "index";
+	}
+
+	@GetMapping("/articles/{id}")
+	public String getUserByUserId(@PathVariable Long id, Model model) {
+		Article article = articleService.findById(id)
+			.orElseThrow(() -> new IllegalStateException("해당 게시글은 존재하지 않습니다."));
+		model.addAttribute("article", article);
+		return "qna/show";
 	}
 
 }
