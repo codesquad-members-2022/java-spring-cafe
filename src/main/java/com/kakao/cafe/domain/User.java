4b69726075a2c3258ebf698c534763fdfb2f9cf5
“kukim” <kukim.dev@gmail.com>
2022-03-03 20:34:01 +0900

    feat: UserRepository 설계와 메모리 기반 구현체
    - UserRepository 인터페이스 기준으로 메모리 기반 구현체 MemoryUserRepository 구현과 테스트 작성
    - MemoryUserRepository 기반 데이터는 User 객체를 저장 Map 형태로 저장
    - UserForm은 InputView의 입력 포맷을 맞춘 객체이다.

---
+++ src/main/java/com/kakao/cafe/domain/User.java
@@ -0,0 +1,41 @@
+package com.kakao.cafe.domain;
+
+public class User {
+
+    private Long id;
+    private final String userId;
+    private final String password;
+    private final String name;
+    private final String email;
+
+    public User(UserForm userForm) {
+        this.userId = userForm.getUserId();
+        this.password = userForm.getPassword();
+        this.name = userForm.getName();
+        this.email = userForm.getEmail();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
---
+++ src/test/java/com/kakao/cafe/repository/MemoryUserRepositoryTest.java
@@ -0,0 +1,130 @@
+package com.kakao.cafe.repository;
+
+import com.kakao.cafe.domain.User;
+import com.kakao.cafe.domain.UserForm;
+import org.junit.jupiter.api.*;
+import org.mockito.internal.matchers.Null;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@DisplayName("MemoryUserRepository 클래스")
+class MemoryUserRepositoryTest {
+
+    MemoryUserRepository repository;
+
+    @BeforeEach
+    void setup() {
+        repository = new MemoryUserRepository();
+    }
+
+    @AfterEach
+    void clear() {
+        repository.clear();
+    }
+
+    @Nested
+    @DisplayName("save 메소드는")
+    class Describe_save {
+
+        @Nested
+        @DisplayName("만약 user 객체가 주어진다면")
+        class Context_with_user {
+
+            @Test
+            @DisplayName("user 객체를 저장한다.")
+            void it_store_user_and_return_user() {
+                User user = new User(new UserForm("testuserid", "1234", "haha", "test@gmail.com"));
+
+                User sut = repository.save(user);
+
+                assertThat(sut.getUserId()).isEqualTo("testuserid");
+                assertThat(sut.getPassword()).isEqualTo("1234");
+                assertThat(sut.getName()).isEqualTo("haha");
+                assertThat(sut.getEmail()).isEqualTo("test@gmail.com");
+            }
+        }
+
+        @Nested
+        @DisplayName("만약 중복된 userID를 가지고 있는 user 객체가 주어진다면")
+        class Context_with_duplicate_user {
+
+            @Test
+            @DisplayName("IllegalArgumentException 예외를 던진다.")
+            void it_throw_IllegalArgumentException() {
+                User user = new User(new UserForm("testuserid", "1234", "haha", "test@gmail.com"));
+                repository.save(user);
+
+                User user2 = new User(new UserForm("testuserid", "1234", "haha", "test@gmail.com"));
+                assertThatThrownBy(() -> repository.save(user2)).isInstanceOf(IllegalArgumentException.class);
+            }
+        }
+
+    }
+
+    @Nested
+    @DisplayName("findAll 메소드는")
+    class Describe_findAll {
+
+        @Nested
+        @DisplayName("파라미터 없이 실행한다면")
+        class Context_without_parameter {
+
+            @Test
+            @DisplayName("모든 user 객체를 리스트로 리턴한다.")
+            void it_store_user_and_return_user() {
+                User user = new User(new UserForm("testuserid", "1234", "haha", "test@gmail.com"));
+                repository.save(user);
+                User user2 = new User(new UserForm("testuserid2", "1234", "haha2", "test@gmail.com"));
+                repository.save(user2);
+
+                List<User> sut = repository.findAll();
+
+                assertThat(sut.size()).isEqualTo(2);
+            }
+        }
+    }
+
+    @Nested
+    @DisplayName("findByUserId 메소드는")
+    class Describe_findByUserId {
+
+        @Nested
+        @DisplayName("존재하는 유저의 id이 주어진다면")
+        class Context_with_userId {
+
+            @Test
+            @DisplayName("해당 유저의 옵셔널 객체를 리턴한다.")
+            void it_return_user() {
+                User user = new User(new UserForm("testuserid", "1234", "haha", "test@gmail.com"));
+                repository.save(user);
+
+                Optional<User> sut = repository.findByUserId("testuserid");
+
+                assertThat(sut).contains(user);
+            }
+        }
+
+        @Nested
+        @DisplayName("존재하지 않는 유저의 id가 주어진다면")
+        class Context_with_not_exist_userId {
+            @Test
+            @DisplayName("비어있는 Optional을 리턴한다")
+            void it_return_user() {
+                User user = new User(new UserForm("testuserid", "1234", "haha", "test@gmail.com"));
+                repository.save(user);
+
+                Optional<User> sut = repository.findByUserId("not_exist");
+
+                assertThat(sut.isEmpty()).isTrue();
+            }
+        }
+    }
+
+}
