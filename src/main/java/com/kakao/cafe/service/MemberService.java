7f2de4852ad354c1a2f28bc2e518de82a03f2e17
vans <spentmny@nate.com>
2022-03-04 16:35:15 +0900

    회원 id에 해당하는 회원 name을 찾는 findOne() 메서드 추가 구현

--- src/main/java/com/kakao/cafe/service/MemberService.java
+++ src/main/java/com/kakao/cafe/service/MemberService.java
@@ -5,19 +5,25 @@ import com.kakao.cafe.repository.MemberRepository;
 import com.kakao.cafe.repository.MemoryMemberRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public class MemberService {
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
 
     //회원가입
-    public Long join(Member member){
+    public Long join(Member member) {
         memberRepository.save(member);
         return member.getId();
     }
 
     //전체 회원목록 조회
-    public List<Member> findMembers(){
+    public List<Member> findMembers() {
         return memberRepository.findAll();
+    }
+
+    //회원 id에 해당하는 회원 name을 찾기
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
     }
 }
