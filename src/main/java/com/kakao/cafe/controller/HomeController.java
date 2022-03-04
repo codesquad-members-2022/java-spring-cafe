4d4edf9cab04e26c4232ed009a6fd98f768cbe19
geombong <shoy1415@gmail.com>
2022-03-05 07:25:54 +0900

    feat. HomeController 클래스 생성

---
+++ src/main/java/com/kakao/cafe/controller/HomeController.java
@@ -0,0 +1,27 @@
+package com.kakao.cafe.controller;
+
+import com.kakao.cafe.entity.Article;
+import com.kakao.cafe.service.ArticleService;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class HomeController {
+
+    private final ArticleService articleService;
+
+    public HomeController(ArticleService articleService) {
+        this.articleService = articleService;
+    }
+
+    @GetMapping("/")
+    public String home(Model model) {
+        List<Article> articles = articleService.findArticles();
+        model.addAttribute("articles", articles);
+        return "index";
+    }
+}
