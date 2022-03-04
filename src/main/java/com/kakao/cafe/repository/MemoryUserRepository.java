ae8e46b188ab56188ec68bd06730a8594d5943b7
Meenzino <86910955+Minzino@users.noreply.github.com>
2022-03-04 16:37:47 +0900

    UserRepository 인터페이스 및 구현체 생성
    Contents
    - UserRepository: User맵을 관리하기 위한 인터페이스
    - MemoryUserRepository: UserRepository의 구현체
    
    Todo
    - 핵심 비즈니스 로직 구현을 위한  UserService 생성

---
+++ src/main/java/com/kakao/cafe/repository/MemoryUserRepository.java
@@ -0,0 +1,36 @@
+package com.kakao.cafe.repository;
+
+import com.kakao.cafe.domain.User;
+
+import java.util.*;
+
+public class MemoryUserRepository implements UserRepository{
+
+    private static Map<Long, User> users = new HashMap<>();
+    private static long sequence = 0L;
+
+
+    @Override
+    public User join(User user) {
+        user.setId(++sequence);
+        users.put(user.getId(), user);
+        return user;
+    }
+
+    @Override
+    public Optional<User> findById(Long id) {
+        return Optional.ofNullable(users.get(id));
+    }
+
+    @Override
+    public Optional<User> findByName(String name) {
+        return users.values().stream()
+                .filter(user -> user.getName().equals(name))
+                .findAny();
+    }
+
+    @Override
+    public List<User> findAll() {
+        return new ArrayList<>(users.values());
+    }
+}
