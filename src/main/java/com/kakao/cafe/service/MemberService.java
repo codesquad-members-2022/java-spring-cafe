1405f8507d4b4a6a2fb3cf0af224edb023133035
vans <spentmny@nate.com>
2022-03-04 16:25:47 +0900

    가입한 회원의 전체 목록을 조회하는 findMembers() 메서드 추가 구현

--- src/main/java/com/kakao/cafe/service/MemberService.java
+++ src/main/java/com/kakao/cafe/service/MemberService.java
@@ -4,6 +4,8 @@ import com.kakao.cafe.domain.Member;
 import com.kakao.cafe.repository.MemberRepository;
 import com.kakao.cafe.repository.MemoryMemberRepository;
 
+import java.util.List;
+
 public class MemberService {
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
@@ -12,5 +14,10 @@ public class MemberService {
     public Long join(Member member){
         memberRepository.save(member);
         return member.getId();
+    }
+
+    //전체 회원목록 조회
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
     }
 }
