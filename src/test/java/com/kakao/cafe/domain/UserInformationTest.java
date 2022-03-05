21bc11e273124e1e656bd00523b5699aba0de189
ikjo93 <auddlr100@naver.com>
2022-03-06 03:26:51 +0900

    test : UserInfromation 테스트 코드 파일 작성

---
+++ src/test/java/com/kakao/cafe/domain/UserInformationTest.java
@@ -0,0 +1,37 @@
+package com.kakao.cafe.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class UserInformationTest {
+
+    private UserInformation userInformation;
+
+    @BeforeEach
+    void setup() {
+        userInformation = new UserInformation("ikjo", "1234", "조명익", "auddlr100@naver.com");
+    }
+
+    @DisplayName("사용자 정보 중 ID 값(userId)이 ikjo와 일치한다.")
+    @Test
+    void 사용자_ID_일치() {
+        // when
+        boolean result = userInformation.hasSameUserId("ikjo");
+
+        // then
+        assertThat(result).isTrue();
+    }
+
+    @DisplayName("사용자 정보 중 비밀번호 값(password)이 1234와 일치한다.")
+    @Test
+    void 사용자_비밀번호_일치() {
+        // when
+        boolean result = userInformation.hasSamePassword("1234");
+
+        // then
+        assertThat(result).isTrue();
+    }
+}
