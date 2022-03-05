a38c0f79455e1cd58e040aa2a92fbcbeaa899ab8
ikjo93 <auddlr100@naver.com>
2022-03-05 23:17:49 +0900

    test : 사용자 정보 수정 기능 관련 UserController 통합 테스트 코드 추가

--- src/test/java/com/kakao/cafe/combination/UserControllerTest.java
+++ src/test/java/com/kakao/cafe/combination/UserControllerTest.java
@@ -15,6 +15,7 @@ import java.util.List;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
@@ -46,10 +47,10 @@ public class UserControllerTest {
         ResultActions resultActions = mockMvc.perform(post("/users").params(userInformationParam));
 
         // then : 사용자 정보 저장 유무 확인을 위해 userId 값 검증
-        resultActions.andExpect(redirectedUrl("/users?joinedUserId=ikjo"));
+        resultActions.andExpect(redirectedUrl("/users"));
     }
 
-    @DisplayName("사용자가 회원 목록을 요청을 했을 때 해당 정보와 함께 model과 view를 반환한다.")
+    @DisplayName("사용자가 회원 목록을 요청을 했을 때 회원 목록과 함께 model과 view를 반환한다.")
     @Test
     void 회원_목록_보기() throws Exception {
         // given
@@ -67,7 +68,7 @@ public class UserControllerTest {
                      .andExpect(model().attribute("users", List.of(userInformation1, userInformation2)));
     }
 
-    @DisplayName("사용자가 특정 회원 프로필을 요청을 했을 때 해당 정보와 함께 model과 view를 반환한다.")
+    @DisplayName("사용자가 특정 회원 프로필을 요청을 했을 때 특정 회원 프로필과 함께 model과 view를 반환한다.")
     @Test
     void 회원_프로필_보기() throws Exception {
         // given
@@ -81,5 +82,41 @@ public class UserControllerTest {
         resultActions.andExpect(status().isOk())
                 .andExpect(view().name("/user/profile"))
                 .andExpect(model().attribute("user", userInformation));
+    }
+
+    @DisplayName("사용자가 회원 정보 수정 화면을 요청 했을 때 model과 /user/updateForm view를 반환한다.")
+    @Test
+    void 회원_정보_수정_화면_보기() throws Exception {
+        // given
+        UserInformation userInformation = new UserInformation("ikjo", "1234", "조명익", "auddlr100@naver.com");
+        userService.join(userInformation);
+
+        // when
+        ResultActions resultActions = mockMvc.perform(get("/users/ikjo/form"));
+
+        // then
+        resultActions.andExpect(status().isOk())
+            .andExpect(view().name("/user/updateForm"))
+            .andExpect(model().attribute("user", userInformation));
+    }
+
+    @DisplayName("사용자가 사용자 정보 수정을 요청하면 사용자 정보를 정상 처리 시 /users로 리다이렉트한다.")
+    @Test
+    void 사용자_정보_수정() throws Exception {
+        // given
+        UserInformation userInformation = new UserInformation("ikjo", "1234", "조명익", "auddlr100@naver.com");
+        userService.join(userInformation);
+
+        MultiValueMap<String, String> userInformationParam = new LinkedMultiValueMap<>();
+        userInformationParam.add("userId", "ikjo");
+        userInformationParam.add("password", "1234");
+        userInformationParam.add("name", "ikjo93");
+        userInformationParam.add("email", "auddlr100@naver.com");
+
+        // when
+        ResultActions resultActions = mockMvc.perform(put("/users/ikjo/update").params(userInformationParam));
+
+        // then
+        resultActions.andExpect(redirectedUrl("/users"));
     }
 }
