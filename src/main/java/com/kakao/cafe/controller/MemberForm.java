96bb4ff93cb269f7bf49d524cb7cffede6db01a6
vans <spentmny@nate.com>
2022-03-06 17:27:40 +0900

    Member 클래스에 email과 password 상태 추가

---
+++ src/main/java/com/kakao/cafe/controller/MemberForm.java
@@ -0,0 +1,5 @@
+package com.kakao.cafe.controller;
+
+public class MemberForm {
+
+}
--- src/main/java/com/kakao/cafe/domain/Member.java
+++ src/main/java/com/kakao/cafe/domain/Member.java
@@ -4,6 +4,8 @@ public class Member {
 
     private Long id;
     private String name;
+    private String email;
+    private String password;
 
     public Long getId() {
         return id;
@@ -19,5 +21,21 @@ public class Member {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
