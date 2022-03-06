15459badb128f99d6f1c378b5b557cef3deb918a
vans <spentmny@nate.com>
2022-03-06 17:46:17 +0900

    User의 이메일, 이름, 비밀번호를 생성해주는 creat()메소드 생성

--- src/main/java/com/kakao/cafe/controller/MemberController.java
+++ src/main/java/com/kakao/cafe/controller/MemberController.java
@@ -1,9 +1,11 @@
 package com.kakao.cafe.controller;
 
+import com.kakao.cafe.domain.Member;
 import com.kakao.cafe.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController {
@@ -18,5 +20,17 @@ public class MemberController {
     @GetMapping("/user/join")
     public String createForm() {
         return "user/form";
+    }
+
+    @PostMapping("/user/join")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setEmail(form.getEmail());
+        member.setName(form.getName());
+        member.setPassword(form.getPassword());
+
+        memberService.join(member);
+
+        return "redirect:/";
     }
 }
