f0aa3bcbb915564ce9ffc1829222133e60226d0b
vans <spentmny@nate.com>
2022-03-06 16:12:01 +0900

    회원가입 form으로 연결해줄 creatForm() 메서드 구현
    home.html과 profile.html의 중복되는 head 부분 fragments.htmel로 변경

--- src/main/java/com/kakao/cafe/controller/MemberController.java
+++ src/main/java/com/kakao/cafe/controller/MemberController.java
@@ -3,6 +3,7 @@ package com.kakao.cafe.controller;
 import com.kakao.cafe.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MemberController {
@@ -12,5 +13,10 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/user/join")
+    public String createForm() {
+        return "user/form";
     }
 }
