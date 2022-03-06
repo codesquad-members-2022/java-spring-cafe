6e7dc772329972713ff991703b2b5565a9b81258
nathan29849 <phs960418@gmail.com>
2022-03-06 12:43:10 +0900

    [step1] refactor : 사용자 목록 자료구조 변경
    HashMap에서 ArrayList로 변경

--- src/main/java/com/kakao/cafe/repository/MemoryUserRepository.java
+++ src/main/java/com/kakao/cafe/repository/MemoryUserRepository.java
@@ -5,31 +5,33 @@ import com.kakao.cafe.domain.User;
 import java.util.*;
 
 public class MemoryUserRepository implements UserRepository {
-    private static Map<Long, User> store = new HashMap<>();
+    private static List<User> store = new ArrayList<>();
     private static long sequence = 0L;
 
     @Override
     public User save(User user) {
         user.setId(++sequence);
-        store.put(user.getId(), user);
+        store.add(user);
         return user;
     }
 
     @Override
     public Optional<User> findById(Long id) {
-        return Optional.ofNullable(store.get(id));
+        return store.stream()
+                .filter(user -> user.getId().equals(id))
+                .findAny();
     }
 
     @Override
     public Optional<User> findByUserId(String userId) {
-        return store.values().stream()
+        return store.stream()
                 .filter(user -> user.getUserId().equals(userId))
                 .findAny();
     }
 
     @Override
     public List<User> findAll() {
-        return new ArrayList<>(store.values()); // 이렇게 바로 주면 안되나? 안된다. (새로운 리스트로 담아서 반환하자.)
+        return new ArrayList<>(store); // 이렇게 바로 주면 안되나? 안된다. (새로운 리스트로 담아서 반환하자.)
         // 반환하는 리스트에서 참조값 공유로 인해 데이터 변경 가능성이 생긴다.
     }
 
