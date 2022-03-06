1c49d396211b0ba42ab33f0311cc4533cae0e6ae
naneun <rmtlrksdp@naver.com>
2022-03-06 23:01:52 +0900

    🚑 UserController 메서드 파라미터 순서 변경

--- src/main/java/com/kakao/cafe/controller/UserController.java
+++ src/main/java/com/kakao/cafe/controller/UserController.java
@@ -2,15 +2,13 @@ package com.kakao.cafe.controller;
 
 import com.kakao.cafe.domain.User;
 import com.kakao.cafe.service.UserService;
-import com.kakao.cafe.service.VolatilityUserService;
-import org.apache.catalina.connector.Request;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
@@ -19,49 +17,51 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Controller
+@RequestMapping("/users")
 public class UserController {
 
     private static final Logger log = LoggerFactory.getLogger(UserController.class);
 
-    private final UserService service;
+    private final UserService userService;
 
-    public UserController(VolatilityUserService service) {
-        this.service = service;
+    public UserController(UserService userService) {
+        this.userService = userService;
     }
 
-    @PostMapping("/user/create")
-    public String signUp(HttpServletRequest request,
-                         User user) {
+    @PostMapping("/join")
+    public String signUp(User user,
+                         HttpServletRequest request) {
 
         logAPIInfo(request);
 
-        service.addUser(user);
+        userService.addUser(user);
         return "redirect:/users";
     }
 
-    @GetMapping("/users")
-    public ModelAndView getUserList(HttpServletRequest request,
-                                    HttpServletResponse response,
-                                    ModelAndView mav) {
+    @GetMapping
+    public ModelAndView getUserList(ModelAndView mav,
+                                    HttpServletRequest request,
+                                    HttpServletResponse response) {
+
         logAPIInfo(request);
         setResponseInfo(response);
 
         mav.setViewName("user/list");
-        mav.addObject("users", service.findAll());
+        mav.addObject("users", userService.findAll());
         return mav;
     }
 
-    @GetMapping("/users/{userId}")
-    public ModelAndView getUserProfile(HttpServletRequest request,
-                                       HttpServletResponse response,
-                                       @PathVariable String userId,
-                                       ModelAndView mav) {
+    @GetMapping("/{userId}")
+    public ModelAndView getUserProfile(@PathVariable String userId,
+                                       ModelAndView mav,
+                                       HttpServletRequest request,
+                                       HttpServletResponse response) {
 
         logAPIInfo(request);
         setResponseInfo(response);
 
         mav.setViewName("user/profile");
-        mav.addObject("user", service.findUser(userId));
+        mav.addObject("user", userService.findUser(userId));
         return mav;
     }
 
@@ -77,6 +77,6 @@ public class UserController {
 
     private void setResponseInfo(HttpServletResponse response) {
         response.setContentType("text/html");
-        response.setCharacterEncoding("utf-8");
+        response.setCharacterEncoding("UTF-8");
     }
 }
