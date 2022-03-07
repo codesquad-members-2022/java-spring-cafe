321488b0cb568d34a877deddfe9889e644dfa337
ByeoungJoon Jeon <juni8453@naver.com>
2022-03-07 12:12:46 +0900

    User 객체 수정
    생성자를 추가하며 userIdx 를 제외하고 모든 인스턴스 변수에 final 을 추가했습니다.
    UserIdx 의 Setter 가 있는 이유는 MemoryUserRepository 에 유저를 저장하는
    변수에서 유저가 등록될 때 마다 번호를 1씩 증가시켜야 하기 때문입니다.

--- src/main/java/com/kakao/cafe/domain/User.java
+++ src/main/java/com/kakao/cafe/domain/User.java
@@ -3,9 +3,15 @@ package com.kakao.cafe.domain;
 public class User {
 
     private long userIdx;
-    private String userName;
-    private String userPassword;
-    private String userEmail;
+    private final String userName;
+    private final String userPassword;
+    private final String userEmail;
+
+    public User(String userName, String userPassword, String userEmail) {
+        this.userName = userName;
+        this.userPassword = userPassword;
+        this.userEmail = userEmail;
+    }
 
     public long getUserIdx() {
         return userIdx;
@@ -18,25 +24,4 @@ public class User {
     public String getUserName() {
         return userName;
     }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public String getUserPassword() {
-        return userPassword;
-    }
-
-    public void setUserPassword(String userPassword) {
-        this.userPassword = userPassword;
-    }
-
-    public String getUserEmail() {
-        return userEmail;
-    }
-
-    public void setUserEmail(String userEmail) {
-        this.userEmail = userEmail;
-    }
-
 }
