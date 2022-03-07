f667708c22f9f72d51abbaa2e45131ad71878eaa
seyeong <seyoung7555@naver.com>
2022-03-07 11:36:34 +0900

    refactor: Controller에서 사용하는 model의 가독성 개선
    - view에 전달하는 model에 무엇을 추가하는 지 잘 알 수 있도록 개선하였습니다.
    - 인자로 전달하던 부분을 따로 변수로 분리했습니다.

--- src/main/java/com/kakao/cafe/controller/IndexController.java
+++ src/main/java/com/kakao/cafe/controller/IndexController.java
@@ -1,9 +1,12 @@
 package com.kakao.cafe.controller;
 
+import com.kakao.cafe.domain.article.Article;
 import com.kakao.cafe.service.ArticleService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class IndexController {
@@ -15,7 +18,8 @@ public class IndexController {
 
     @GetMapping("/")
     public String index(Model model) {
-        model.addAttribute("articleList", articleService.findAllDesc());
+        List<Article> articleList = articleService.findAllDesc();
+        model.addAttribute("articleList", articleList);
         return "index";
     }
 }
