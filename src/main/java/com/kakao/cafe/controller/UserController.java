542eaafce4c9684ea1bb62f1e31e24cf7a790b23
“kukim” <kukim.dev@gmail.com>
2022-03-07 17:18:29 +0900

    feat: UserController 기능 구현
    - GET /users : 회원 목록 전체 조회
    - GET /users/{userId} : 특정 회원 프로필 조회
    - GET /users/form : 회원가입 폼
    - POST /users : 회원가입

---
+++ src/main/java/com/kakao/cafe/controller/UserController.java
@@ -0,0 +1,53 @@
+package com.kakao.cafe.controller;
+
+import com.kakao.cafe.domain.User;
+import com.kakao.cafe.domain.UserForm;
+import com.kakao.cafe.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public String list(Model model) {
+        List<User> users = userService.findUsers();
+        model.addAttribute("users", users);
+        return "user/list";
+    }
+
+    @GetMapping("/{userId}")
+    public String showUser(@PathVariable String userId, Model model) {
+        User user = userService.findUser(userId);
+        model.addAttribute("user", user);
+        return "user/profile";
+    }
+
+    @GetMapping("/form")
+    public String createForm() {
+        return "user/form";
+    }
+
+    @PostMapping
+    public String create(UserForm userForm) {
+        User user = new User(userForm);
+        userService.join(user);
+        return "redirect:/users";
+    }
+
+}
