b22dd79dbb60571c499d874b8f8c64bb77940e78
Jaehong Choe <qnalsfj@gmail.com>
2022-03-08 14:29:49 +0900

    Article, ArticleDto 클래스 추가
    - ArticleDto : 브라우저에서 입력된 게시글 정보를 전달받을 클래스.
    - Article : 입력받은 게시글 정보를 검증 후 데이터베이스에 옮겨담기 위한 클래스.

---
+++ src/main/java/com/kakao/cafe/controller/ArticleDto.java
@@ -0,0 +1,32 @@
+package com.kakao.cafe.controller;
+
+public class ArticleDto {
+
+    private String writer;
+    private String title;
+    private String contents;
+
+    public String getWriter() {
+        return writer;
+    }
+
+    public void setWriter(String writer) {
+        this.writer = writer;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContents() {
+        return contents;
+    }
+
+    public void setContents(String contents) {
+        this.contents = contents;
+    }
+}
---
+++ src/main/java/com/kakao/cafe/domain/Article.java
@@ -0,0 +1,28 @@
+package com.kakao.cafe.domain;
+
+import com.kakao.cafe.controller.ArticleDto;
+
+public class Article {
+
+    private final String writer;
+    private final String title;
+    private final String contents;
+
+    public Article(ArticleDto form) {
+        writer = form.getWriter();
+        title = form.getTitle();
+        contents = form.getContents();
+    }
+
+    public String getWriter() {
+        return writer;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getContents() {
+        return contents;
+    }
+}
