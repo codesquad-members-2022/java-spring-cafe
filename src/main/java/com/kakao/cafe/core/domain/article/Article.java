5839ead77004c9f5ce38f5c69a7bc15af58756ea
jun <devjun1023@gmail.com>
2022-03-08 17:17:32 +0900

    Article 엔티티 생성자
    Contents
    - 많은 컬럼을 명확히 구분하기 위해 Builder 생성
    
    Todo
    - html 수정

--- src/main/java/com/kakao/cafe/core/domain/article/Article.java
+++ src/main/java/com/kakao/cafe/core/domain/article/Article.java
@@ -11,4 +11,73 @@ public class Article {
     private LocalDateTime createAt;
     private LocalDateTime lastModifiedAt;
     private int viewCount;
+
+    public Article(int id, String title, String content, String writer, LocalDateTime createAt, LocalDateTime lastModifiedAt, int viewCount) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.writer = writer;
+        this.createAt = createAt;
+        this.lastModifiedAt = lastModifiedAt;
+        this.viewCount = viewCount;
+    }
+
+    public static class Builder {
+        private int id;
+        private String title;
+        private String content;
+        private String writer;
+        private LocalDateTime createAt;
+        private LocalDateTime lastModifiedAt;
+        private int viewCount;
+
+        public Builder(Article article) {
+            this.id = article.id;
+            this.title = article.title;
+            this.content = article.content;
+            this.writer = article.writer;
+            this.createAt = article.createAt;
+            this.lastModifiedAt = article.lastModifiedAt;
+            this.viewCount = article.viewCount;
+        }
+
+        public Article.Builder id(int id) {
+            this.id = id;
+            return this;
+        }
+
+        public Article.Builder title(String title) {
+            this.title = title;
+            return this;
+        }
+
+        public Article.Builder content(String content) {
+            this.content = content;
+            return this;
+        }
+
+        public Article.Builder writer(String writer) {
+            this.writer = writer;
+            return this;
+        }
+
+        public Article.Builder createAt(LocalDateTime createAt) {
+            this.createAt = createAt;
+            return this;
+        }
+
+        public Article.Builder lastModifiedAt(LocalDateTime lastModifiedAt) {
+            this.lastModifiedAt = lastModifiedAt;
+            return this;
+        }
+
+        public Article.Builder viewCount(int viewCount) {
+            this.viewCount = viewCount;
+            return this;
+        }
+
+        public Article build() {
+            return new Article(id, title, content, writer, createAt, lastModifiedAt, viewCount);
+        }
+    }
 }
