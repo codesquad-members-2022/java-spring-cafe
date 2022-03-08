73e955bcbc7205f186d1ab7d4193b4ab0542ed6c
donggi-lee-bit <devdonggilee@gmail.com>
2022-03-09 03:23:59 +0900

    회원 데이터를 저장한 구현 클래스 추가
    - User 객체를 받아서 저장하는 save 메서드를 구현하였습니다.
    - User 객체가 저장된 List를 순회하여 매개변수로 넘어온 name과 같은 게 있으면 해당 name을 반환하거나 null을 반환하는 findByName 메서드를 구현하였습니다.
    - 저장된 user List를 반환하는 findAll 메서드를 구현하였습니다.

--- src/main/java/com/kakao/cafe/repository/UserRepository.java
+++ src/main/java/com/kakao/cafe/repository/UserRepository.java
@@ -7,9 +7,7 @@ import java.util.Optional;
 
 public interface UserRepository {
 
-    User save(User user);
-
-    Optional<User> findById(Long id);
+    void save(User user);
 
     Optional<User> findByName(String name);
 
