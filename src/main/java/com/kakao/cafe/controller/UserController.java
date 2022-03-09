21709982ca152d4309576e3b8635b962cbd25b93
nak253 <nwon4413@naver.com>
2022-03-09 15:07:33 +0900

    UserController클래스 profile메서드 추가
    - 프로필 조회 기능 구현을 위해서 UserService에 findOne 메서드를 추가하였다.
    - 브라우저에서 프로필을 확인할 수 있도록 profile 메서드를 작성하였다.

--- src/main/java/com/kakao/cafe/controller/UserController.java
+++ src/main/java/com/kakao/cafe/controller/UserController.java
@@ -5,6 +5,7 @@ import com.kakao.cafe.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -33,5 +34,11 @@ public class UserController {
     public String create(User user) {
         userService.join(user);
         return "redirect:/users";
+    }
+
+    @GetMapping("/users/{userId}")
+    public String profile(@PathVariable("userId") String userId, Model model) {
+        model.addAttribute("user", userService.findOne(userId).get());
+        return "user/profile";
     }
 }
--- src/main/java/com/kakao/cafe/service/UserService.java
+++ src/main/java/com/kakao/cafe/service/UserService.java
@@ -5,6 +5,7 @@ import com.kakao.cafe.repository.MemoryUserRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -20,6 +21,9 @@ public class UserService {
         return user.getName();
     }
 
+    public Optional<User> findOne(String userId) {
+        return memoryUserRepository.findByUserId(userId);
+    }
     public List<User> findUsers() {
         return memoryUserRepository.findAll();
     }
