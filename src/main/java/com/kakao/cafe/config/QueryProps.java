a27044dc753cbdaaf14d3d6fb0ba3cb00066a7ec
Kang Changgu (Miller) <rkckr1@naver.com>
2022-03-09 10:18:37 +0900

    deploy: heroku 배포를 위한 system.properties 추가

--- src/main/java/com/kakao/cafe/config/QueryProps.java
+++ src/main/java/com/kakao/cafe/config/QueryProps.java
@@ -12,13 +12,11 @@ import org.yaml.snakeyaml.Yaml;
 @Component
 public class QueryProps {
 
-    private static final String FILENAME = "query.yml";
-
     private Map<String, String> props = new HashMap<>();
 
     public QueryProps() {
         try {
-            ClassPathResource resource = new ClassPathResource(FILENAME);
+            ClassPathResource resource = new ClassPathResource("query.yml");
             props = new Yaml().load(new FileReader(resource.getFile()));
 
         } catch (IOException e) {
