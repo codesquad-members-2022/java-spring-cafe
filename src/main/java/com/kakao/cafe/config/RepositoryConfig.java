72954a13110220199e9c7384be325e7753bc3622
dongkyu <dongkyu.parkk@gmail.com>
2022-03-10 02:56:23 +0900

    refactor: AppConfig.class -> RepositoryConfig.class 클래스 파일명 변경

--- src/main/java/com/kakao/cafe/config/RepositoryConfig.java
+++ src/main/java/com/kakao/cafe/config/RepositoryConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class AppConfig {
+public class RepositoryConfig {
 
     @Bean
     public UserRepository userRepository() {
