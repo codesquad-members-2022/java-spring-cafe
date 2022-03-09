08eb265ab6b0d29c47552e394d91115a2bb3788d
tokaji <fpzk5656@gmail.com>
2022-03-09 21:26:39 +0900

    refector: -회원 목록 페이지에서 '메인 페이지'를 클릭하면 메인 페이지로 돌아가도록 하였습니다. -회원 목록 페이지에서 회원 가입을 누르면 회원 가입 화면으로 가도록 하였습니다.

--- src/test/java/com/kakao/cafe/service/UserServiceTest.java
+++ src/test/java/com/kakao/cafe/service/UserServiceTest.java
@@ -10,7 +10,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 
 class UserServiceTest {
 
