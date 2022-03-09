23337a8d2079239d53877235730588b9b21f45ad
rkolx <olx.rko.o@gmail.com>
2022-03-09 16:56:26 +0900

    feat : UserRepository 인터페이스 및 MemoryUserRepository 생성

---
+++ src/main/java/com/kakao/cafe/repository/MemoryUserRepository.java
@@ -0,0 +1,43 @@
+package com.kakao.cafe.repository;
+
+import com.kakao.cafe.domain.User;
+import org.springframework.stereotype.Repository;
+
+import java.util.*;
+
+@Repository
+public class MemoryUserRepository implements UserRepository{
+
+    private static Map<Long, User> store = new HashMap<>();
+    private static Long sequence = 0L;
+
+    @Override
+    public User save(User user) {
+        user.setId(++sequence);
+        store.put(user.getId(), user);
+        return user;
+    }
+
+    @Override
+    public Optional<User> findById(Long id) {
+        return Optional.ofNullable(store.get(id));
+    }
+
+    @Override
+    public Optional<User> findByNickname(String nickname) {
+        return store.values().stream()
+                .filter(user -> user.getNickname().equals(nickname))
+                .findAny();
+    }
+
+    @Override
+    public Optional<User> findByEmail(String email) {
+        return Optional.ofNullable(store.get(email));
+    }
+
+    @Override
+    public List<User> findAll() {
+        return new ArrayList<>(store.values());
+    }
+
+}
