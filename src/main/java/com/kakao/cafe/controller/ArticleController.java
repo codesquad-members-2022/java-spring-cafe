34c70c67b61112d5a0be9937906c2b3b2b0d5c13
Kyle Choi <tmdfl1027@gmail.com>
2022-03-09 15:59:46 +0900

    [step2] ArticleController 클래스 메서드 작성
    ArticleController 클래스에 각각 질문 작성페이지 GET, 질문 작성 POST, 질문 상세보기 GET Mapping 메서드 작성.
    이후 필요한 도메인 객체(Article)과 리포지토리 및 서비스 구현 필요.

---
+++ src/main/java/com/kakao/cafe/controller/ArticleController.java
@@ -0,0 +1,36 @@
+package com.kakao.cafe.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class ArticleController {
+    @GetMapping("/questions")
+    public String createForm() {
+        return "../templates/form";
+    }
+
+    @PostMapping("/questions")
+    public String create(ArticleForm form) {
+        Article article = new Article(form);
+        articleService.create(article);
+        return "redirect:/";
+    }
+
+    @GetMapping("/articles/{articleId}")
+    public String viewArticle(@PathVariable("articleId") int articleId, Model model) {
+        Article selectedArticle;
+        Optional<Article> foundArticle = articleService.findById(articleId);
+        if (foundArticle.isPresent()) {
+            selectedArticle = foundArticle.get();
+            model.addAttribute("article", selectedArticle);
+            return "/qna/show";
+        }
+        return null; // 잘못된 접근 페이지 리다이렉트
+    }
+
+
+}
