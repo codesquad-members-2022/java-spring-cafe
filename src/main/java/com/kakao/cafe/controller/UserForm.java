273957a6e6de83fe2abfdfbbeff8084f5a23b6c7
leekm0310 <leekm310@gmail.com>
2022-03-09 16:39:59 +0900

    refactor: UserForm의 setter 제거 및 생성자 삽입
    * setter를 삭제하고 생성자를 통해서 처리할 수 있도록 수정했습니다.

--- src/main/java/com/kakao/cafe/controller/UserForm.java
+++ src/main/java/com/kakao/cafe/controller/UserForm.java
@@ -7,35 +7,26 @@ public class UserForm {
     private String name;
     private String email;
 
-    public String getUserId() {
-        return userId;
+    public UserForm(String userId, String password, String name, String email) {
+        this.userId = userId;
+        this.password = password;
+        this.name = name;
+        this.email = email;
     }
 
-    public void setUserId(String userId) {
-        this.userId = userId;
+    public String getUserId() {
+        return userId;
     }
 
     public String getPassword() {
         return password;
     }
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getEmail() {
         return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
     }
 }
