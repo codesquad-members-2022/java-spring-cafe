2787023460cb8b136f160dafff51bb27c5f57135
nak253 <nwon4413@naver.com>
2022-03-09 11:52:59 +0900

    UserController 클래스 작성
    - 회원가입하는 기능을 구현하였다.

---
+++ src/main/java/com/kakao/cafe/controller/UserController.java
@@ -0,0 +1,37 @@
+package com.kakao.cafe.controller;
+
+import com.kakao.cafe.domain.User;
+import com.kakao.cafe.service.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class UserController {
+
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/users")
+    public String list(Model model) {
+        List<User> users = userService.findUsers();
+        model.addAttribute("users",users);
+        return "user/list";
+    }
+
+    @GetMapping("/users/form")
+    public String form() {
+        return "user/form";
+    }
+
+    @GetMapping("/users/create")
+    public String create(User user) {
+        userService.join(user);
+        return "redirect:/users";
+    }
+}
