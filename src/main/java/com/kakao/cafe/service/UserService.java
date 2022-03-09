07afc68b65fd76889e721013abed03d3ecf3c4b8
Meenzino <86910955+Minzino@users.noreply.github.com>
2022-03-09 23:23:12 +0900

    비즈니스 로직을 위한 UserService구현
    Contents
    - User 도메인 관련 비즈니스 로직을 담당하는 클래스 구현
    - join: 가입을 위한 메서드
    - validateDuplicateUser: 가입시 user중복처리
    - findMembers,findOne: 회원 조회
    
    Todo
    - UserService 인터페이스의 구현체인 MemmoryUserRepository 생성

---
+++ src/main/java/com/kakao/cafe/service/UserService.java
@@ -0,0 +1,45 @@
+package com.kakao.cafe.service;
+
+import com.kakao.cafe.domain.User;
+import com.kakao.cafe.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.Optional;
+
+
+public class UserService {
+
+    private final UserRepository userRepository;
+
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+    public Long join(User user){
+        validateDuplicateUser(user);
+        userRepository.join(user);
+        return user.getId();
+    }
+
+    private void validateDuplicateUser(User user) {
+        userRepository.findByName(user.getName())
+                .ifPresent(u -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+
+    public List<User> findMembers(){
+        return userRepository.findAll();
+    }
+
+    public Optional<User> findOne(Long userId){
+        return userRepository.findById(userId);
+    }
+
+
+}
