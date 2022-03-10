a1eec3196f31f561161c33d0be7c2eef2259da79
Seung-Wan Jeong <sjeong4776@gmail.com>
2022-03-10 19:18:40 +0900

    Remove UserRepository interface
    * 미션 1단계 요구사항 구현에 불필요한 부분을 제거하였다
    * OnMemoryUserRepository를 UserRepository로 이름 변경

--- README.md
+++ README.md
@@ -9,16 +9,19 @@
 - [x] 회원 가입 기능 구현
   - 회원 가입 양식: /users/form.html
   - 요청 메서드: POST /users
+  - 전달받은 값으로 User 객체를 생성하여 저장
+    - getter, setter 메소드를 생성한다
+  - 생성된 User 객체는 ArrayList로 구현한 사용자 목록에 저장한다.
   - 회원 가입 후: 유저 목록으로 리다이렉트 (redirect:/users)
 - [x] 회원 조회 기능 구현
   - 요청 메서드: GET /users
   - 회원 목록 뷰 템플릿: /users/list.html
 - [x] 회원 프로필 조회 기능 구현
+  - 회원 목록에서 닉네임을 누르면 프로필 페이지로 이동하도록 한다. 
   - 요청 메서드: GET /users/{userId}
   - 회원 프로필 뷰 템플릿: /users/profile.html
 - [x] 템플릿을 이용하여 HTML 파일들의 중복 제거
 - [x] DB는 사용하지 않는다.
-  - 단, 이후 DB를 사용하도록 변경할 수 있어야 한다.
 
 ### 구현과정 
 
--- src/main/java/kr/codesquad/cafe/users/UserRepository.java
+++ src/main/java/kr/codesquad/cafe/users/UserRepository.java
@@ -1,20 +1,50 @@
 package kr.codesquad.cafe.users;
 
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.atomic.AtomicLong;
 
-public interface UserRepository {
+@Repository
+public class UserRepository {
 
-    User save(User user);
+    private static final ConcurrentMap<Long, User> repository = new ConcurrentHashMap<>();
+    private static final AtomicLong nextId = new AtomicLong(1);
 
-    Optional<User> findById(Long id);
+    public User save(User user) {
+        user.setId(nextId.getAndIncrement());
+        repository.put(user.getId(), user);
 
-    Optional<User> findByUserId(String userId);
+        return user;
+    }
 
-    Optional<User> findByName(String name);
+    public Optional<User> findById(Long id) {
+        return Optional.ofNullable(repository.get(id));
+    }
 
-    Optional<User> findByEmail(String email);
+    public Optional<User> findByUserId(String userId) {
+        return repository.values().stream()
+                .filter(user -> user.userIdIs(userId))
+                .findAny();
+    }
 
-    List<User> findAll();
+    public Optional<User> findByName(String name) {
+        return repository.values().stream()
+                .filter(user -> user.nameIs(name))
+                .findAny();
+    }
 
+    public Optional<User> findByEmail(String email) {
+        return repository.values().stream()
+                .filter(user -> user.emailIs(email))
+                .findAny();
+    }
+
+    public List<User> findAll() {
+        return new ArrayList<>(repository.values());
+    }
 }
