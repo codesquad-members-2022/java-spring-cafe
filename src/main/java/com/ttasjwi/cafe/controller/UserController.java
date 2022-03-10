7b9a16ae4594a9523cebb632e3ac4ae43cf3fce9
ttasjwi <ttasjwi920@gmail.com>
2022-03-10 22:11:24 +0900

    UserController - list 메서드 및 테스트 코드 추가(회원목록 구현 완료)
    회원 목록 페이지로 맵핑되는 list 메서드 추가,
    
    - `list(...)` : GET 메서드로 `/users`가 맵핑되면, 회원 전체 list를 Model에 담고 `/users/userList` 반환
    - 테스트코드도 추가했다.

--- src/main/java/com/ttasjwi/cafe/controller/UserController.java
+++ src/main/java/com/ttasjwi/cafe/controller/UserController.java
@@ -1,15 +1,35 @@
 package com.ttasjwi.cafe.controller;
 
+import com.ttasjwi.cafe.domain.User;
+import com.ttasjwi.cafe.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/users")
 public class UserController {
 
+    private final UserRepository userRepository;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = new UserRepository();
+    }
+
     @GetMapping("/new")
     public String createForm() {
         return "/users/createUserForm";
+    }
+
+    @GetMapping
+    public String list(Model model) {
+        List<User> list = userRepository.findAll();
+        model.addAttribute("users", list);
+        return "/users/userList";
     }
 }
--- src/test/java/com/ttasjwi/cafe/controller/UserControllerTest.java
+++ src/test/java/com/ttasjwi/cafe/controller/UserControllerTest.java
@@ -1,25 +1,25 @@
 package com.ttasjwi.cafe.controller;
 
+import com.ttasjwi.cafe.repository.UserRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 @WebMvcTest(controllers = UserController.class)
-@ExtendWith(SpringExtension.class)
 class UserControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @MockBean
+    private UserRepository userRepository;
 
     @Test
     @DisplayName("\"/users/new\"로 요청하면 \"/users/createUserForm\" 이 반환된다.")
@@ -29,5 +29,15 @@ class UserControllerTest {
         mockMvc.perform(get(requestUrl))
                 .andExpect(status().isOk())
                 .andExpect(view().name("/users/createUserForm"));
+    }
+
+    @Test
+    @DisplayName("\"/users\"로 요청하면 \"/users/userList\" 가 반환된다.")
+    void listTest() throws Exception {
+        String requestUrl = "/users";
+
+        mockMvc.perform(get(requestUrl))
+                .andExpect(status().isOk())
+                .andExpect(view().name("/users/userList"));
     }
 }
