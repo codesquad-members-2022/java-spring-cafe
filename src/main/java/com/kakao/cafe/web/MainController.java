97c612ec2465a54734f766cd7df119d3d2e44a3f
jiwoo <zbqmgldjfh@gmail.com>
2022-03-10 21:39:24 +0900

    feat: Main 화면에서 로그인 유무에 따른 화면 구현하기

--- src/main/java/com/kakao/cafe/web/MainController.java
+++ src/main/java/com/kakao/cafe/web/MainController.java
@@ -1,13 +1,17 @@
 package com.kakao.cafe.web;
 
+import com.kakao.cafe.domain.User;
 import com.kakao.cafe.service.ArticleService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 @Controller
 public class MainController {
-
+    private static final String LOGIN_USER_KEY = "SESSIONED_USER";
     private final ArticleService articleService;
 
     public MainController(ArticleService articleService) {
@@ -15,8 +19,21 @@ public class MainController {
     }
 
     @GetMapping("/")
-    public String main(Model model) {
+    public String main(HttpServletRequest request, Model model) {
+        HttpSession session = request.getSession(false);
         model.addAttribute("articles", articleService.findArticles());
+
+        if (session == null) {
+            return "index";
+        }
+
+        User loginUser = (User) session.getAttribute(LOGIN_USER_KEY);
+
+        if (loginUser == null) {
+            return "index";
+        }
+
+        model.addAttribute("user", loginUser);
         return "index";
     }
 }
