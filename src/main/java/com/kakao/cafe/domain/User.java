7fcb712be53a11f7d80ccac92eac7e39f258ed18
donggi-lee-bit <devdonggilee@gmail.com>
2022-03-10 16:30:00 +0900

    User 클래스에서 setter를 사용하지 않고 생성자에서 받도록 수정
    - setter 를 사용하지 않을 방법을 찾다가 회원 정보는 이름, 이메일, 패스워드 전부가 입력되어야 회원 정보가 생성되기에 생성자에서 그 일을 해주어도 될 것 같다는 생각에 setter를 지우고 생성자에서 그 일을 하도록 수정하였습니다.

--- src/main/java/com/kakao/cafe/domain/User.java
+++ src/main/java/com/kakao/cafe/domain/User.java
@@ -1,41 +1,20 @@
 package com.kakao.cafe.domain;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 public class User {
 
-    private Long id;
     private String name;
     private String email;
     private String password;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
+    public User(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
     }
 
     public String getName() {
         return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
     }
 }
