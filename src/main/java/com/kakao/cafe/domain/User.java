50ef974a7e416bb3d271903555bee6bb256cc1d6
leekm0310 <leekm310@gmail.com>
2022-03-10 23:30:23 +0900

    refactor: User 클래스에 id를 비교할 수 있는 메소드 추가
    * user가 스스로 행동할 수 있게 만들어달라는 코멘트에 대해서 생각하다가 user안에 메소드를 만들어 user가 직접 값을 넘겨줄 수 있도록 하였습니다.

--- src/main/java/com/kakao/cafe/domain/User.java
+++ src/main/java/com/kakao/cafe/domain/User.java
@@ -38,4 +38,8 @@ public class User {
     public String getEmail() {
         return email;
     }
+
+    public boolean checkIfTheSameOrNot(String userId){
+        return this.userId.equals(userId);
+    }
 }
