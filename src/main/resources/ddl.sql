02002d0a7a0d7e6b64511da560fff4daa3d30593
Minseok-Choi <cms05041@gmail.com>
2022-03-10 15:39:03 +0900

    chore : h2database, jdbc dependency 추가, application.properties dataSource 설정

---
+++ src/main/resources/ddl.sql
@@ -0,0 +1,6 @@
+create table cafe_user (
+                        userId varchar(255) primary key,
+                        password varchar(255) not null,
+                        name varchar(255),
+                        email varchar(255)
+)
