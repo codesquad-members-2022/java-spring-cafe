ec494c917a7ccd01fdb3aad439e5e6a930dc510d
donggi-lee-bit <devdonggilee@gmail.com>
2022-03-12 20:45:35 +0900

    회원 프로필을 요청하는 메서드 추가
    - 회원 프로필을 요청하기 위해 @GetMapping을 추가하였습니다.
    - url을 통해 전달된 사용자 이름을 @PathVariable 애노테이션을 이용해 전달 받았습니다.

--- src/main/java/com/kakao/cafe/service/UserService.java
+++ src/main/java/com/kakao/cafe/service/UserService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -23,5 +24,9 @@ public class UserService {
 
     public List<User> findUsers() {
         return userRepository.findAll();
+    }
+
+    public Optional<User> findUser(String name) {
+        return userRepository.findByName(name);
     }
 }
