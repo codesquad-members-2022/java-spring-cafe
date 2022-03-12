1c957d50c05bfcbff1c56dadab9cf6179711dcb3
ttasjwi <ttasjwi920@gmail.com>
2022-03-12 10:51:06 +0900

    ArticleController - create 메서드 추가(기능 미완성)
    - 게시글 등록 폼에서 POST 요청이 들어왔을 때 맵핑되는 메서드를 추가했다.
    - 게시글의 파라미터들을 읽고, Article로 맵핑하여 생성해준다.
    - 작성자는 현재 로그인 기능이 별도로 존재하지 않으므로 임의로 지정한 사용자명을 세팅한다.
    - 날짜는 작성시점을 기준으로 LocalDate.now()를 사용한다.
    - viewName으로 "redirect:/"이 반환된다. 즉 게시글을 작성되면 홈으로 리다이렉트 된다.
    - 게시글을 실제로 저장계층에 등록하는 로직은 수행하지 않았다.

--- src/main/java/com/ttasjwi/cafe/controller/ArticleController.java
+++ src/main/java/com/ttasjwi/cafe/controller/ArticleController.java
@@ -1,16 +1,34 @@
 package com.ttasjwi.cafe.controller;
 
+import com.ttasjwi.cafe.domain.Article;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDate;
 
 @Controller
 @RequestMapping("/articles")
 public class ArticleController {
 
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
     @GetMapping("/new")
     public String createArticleForm() {
         return "/articles/createArticleForm";
+    }
+
+    @PostMapping("/new")
+    public String createArticle(@ModelAttribute Article article) {
+        article.setWriter("anonymous User");
+        article.setRegDate(LocalDate.now());
+        log.info("new Article={}",article);
+        return "redirect:/";
     }
 
 }
