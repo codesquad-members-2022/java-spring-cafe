cfa1c1308df6f7a8cb4c291a68ea79fda7ea253d
donggi-lee-bit <devdonggilee@gmail.com>
2022-03-12 17:41:43 +0900

    회원가입 요청을 처리할 메서드 추가
    - 회원가입 요청을 보내기 위해 @PostMapping 으로 매핑한 메서드를 추가하였습니다.
    - user 정보를 userform이라는 객체에 담아 받기 위해 UserForm 클래스를 추가하였습니다.
    - 사용자가 입력한 회원 정보를 받아 repository에 저장하는 작업을 처리하는 UserService 클래스를 추가하였습니다.

--- src/main/java/com/kakao/cafe/controller/UserController.java
+++ src/main/java/com/kakao/cafe/controller/UserController.java
@@ -1,13 +1,31 @@
 package com.kakao.cafe.controller;
 
+import com.kakao.cafe.domain.User;
+import com.kakao.cafe.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
 
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
     @GetMapping("/user/new")
     public String createForm() {
         return "/user/form";
+    }
+
+    @PostMapping("/user/new")
+    public String create(UserForm form) {
+        User user = new User(form.getName(), form.getEmail(), form.getPassword());
+        userService.join(user);
+        return "redirect:/users";
     }
 }
