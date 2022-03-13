148de54b5b09fea750b538c8abacaa2c4f56bb4b
KTH-96 <taehyun960@gmail.com>
2022-03-13 21:45:56 +0900

    test: JdbcTest를 사용하여 로직 검증, BDD스타일로 컨트롤러 테스트

--- src/test/java/com/kakao/cafe/domain/user/UserRepositoryTest.java
+++ src/test/java/com/kakao/cafe/domain/user/UserRepositoryTest.java
@@ -1,61 +1,99 @@
 package com.kakao.cafe.domain.user;
 
+import com.kakao.cafe.web.dto.UserJoinDto;
+import com.kakao.cafe.web.dto.UserUpdateDto;
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.jdbc.Sql;
+
+import javax.sql.DataSource;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 
+@JdbcTest//transaction 포함
+@Sql({"/schema.sql", "/test_data.sql"})
 class UserRepositoryTest {
     Logger log = LoggerFactory.getLogger(UserRepositoryTest.class);
-    UserRepository userRepository;
+    @Autowired
+    DataSource dataSource;
+
+    JdbcUserRepository repository;
+
+
     @BeforeEach
     void setUp() {
-        log.info("setUp");
-        userRepository = new UserRepository();
+        repository = new JdbcUserRepository(dataSource);
     }
 
-    @AfterEach
-    void clear() {
-        log.info("after");
-        userRepository.clear();
-    }
 
     @Test
-    @DisplayName("userRepository에서 save테스트")
+    @DisplayName("jdbc save")
     void save() {
-        //given 준비
-        User userA = new User("userID", "password", "rlaxogus", "r@gmail.com");
-        User userB = new User("userID2", "password2", "rlaxogus2", "r@gmail.com2");
-        //when 실행
-        userRepository.save(userA);
-        userRepository.save(userB);
-        //then 검증
-        assertThat(userRepository.findById(userA.getId())).isEqualTo(userA);
-        log.info("findById Id = {}, getUserId = {}"
-                , userRepository.findById(userA.getId()).getId()
-                , userRepository.findById(userA.getId()).getUserId());
-        assertThat(userRepository.findById(userB.getId())).isEqualTo(userB);
-        log.info("findById Id = {}, getUserId = {}"
-                , userRepository.findById(userB.getId()).getId()
-                , userRepository.findById(userB.getId()).getUserId());
+        //given
+        User user = new User("save", "save", "save", "save");
+        repository.save(user);
+        //when
+        User saveUser = repository.findByUserId("save");
+        //then
+        assertThat(saveUser.getId()).isEqualTo(4L);
+        assertThat(saveUser.getUserId()).isEqualTo("save");
+        assertThat(saveUser.getName()).isEqualTo("save");
+        assertThat(saveUser.getEmail()).isEqualTo("save");
     }
 
     @Test
-    @DisplayName("userRepository에서 findAll 사이즈 테스트")
-    void findAll() {
-        //given 준비
-        User userA = new User("userID", "password", "rlaxogus", "r@gmail.com");
-        User userB = new User("userID2", "password2", "rlaxogus2", "r@gmail.com2");
+    @DisplayName("jdbc findById")
+    void findById() {
+        //given
+        log.info("test_data.sql 파일에 테스트 데이터 생성");
+        //when
+        User userA = repository.findById(1L);
+        User userB = repository.findById(2L);
+        User userC = repository.findById(3L);
+        //then
+        assertThat(userA.getUserId()).isEqualTo("test1");
+        assertThat(userB.getUserId()).isEqualTo("test2");
+        assertThat(userC.getUserId()).isEqualTo("test3");
+    }
+    @Test
+    @DisplayName("jdbc findByUserId")
+    void findByUserId() {
         //when 실행
-        userRepository.save(userA);
-        userRepository.save(userB);
+        User userA = repository.findByUserId("test1");
+        User userB = repository.findByUserId("test2");
+        User userC = repository.findByUserId("test3");
         //then 검증
-        assertThat(userRepository.findAll().size()).isEqualTo(2);
+        assertThat(userA.getName()).isEqualTo("name1");
+        assertThat(userB.getName()).isEqualTo("name2");
+        assertThat(userC.getName()).isEqualTo("name3");
+    }
+    @Test
+    @DisplayName("jdbc findAll")
+    void findAll() {
+        //when 실행
+        List<User> userList = repository.findAll();
+        //then 검증
+        assertThat(userList.size()).isEqualTo(3);
+    }
+    @Test
+    @DisplayName("jdbc update")
+    void update() {
+        //given 준비
+        User user = repository.findByUserId("test1");
+        UserUpdateDto dto = new UserUpdateDto("update", "updateName", "updateEmail");
+        //when 실행
+        repository.updateUser(user.getId(), dto);
+        User updateUser = repository.findByUserId("test1");
+        //then 검증
+        assertThat(updateUser.getName()).isEqualTo("updateName");
     }
 }
