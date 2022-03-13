07e1b409f1e4a352b9a4bc0c138c17612d126e8d
KTH-96 <taehyun960@gmail.com>
2022-03-13 21:41:02 +0900

    docs : 테스트를 위한 sql설정 파일
    test_data 파일로 테스트 테이터 자동화

---
+++ src/main/resources/test_data.sql
@@ -0,0 +1,7 @@
+INSERT INTO `user` VALUES ('1', 'test1', 'password1', 'name1', 'email1');
+INSERT INTO `user` VALUES ('2', 'test2', 'password2', 'name2', 'email2');
+INSERT INTO `user` VALUES ('3', 'test3', 'password3', 'name3', 'email3');
+
+INSERT INTO post VALUES ('1', 'test1', 'title1', 'content1', '1111-11-11');
+INSERT INTO post VALUES ('2', 'test2', 'title2', 'content2', '1111-11-11');
+INSERT INTO post VALUES ('3', 'test3', 'title3', 'content3', '1111-11-11');
