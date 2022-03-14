3a1b2f8bd9603fac89d27244c1cfac68052931f5
nak253 <nwon4413@naver.com>
2022-03-14 23:56:59 +0900

    ArticleController클래스 글쓰기 기능 구현
    - 글쓰기 기능을 구현하기 위해서 create()를 작성하였다.
    - 기능구현을 위해서 ArticleService를 필드로 선언하고 생성자를 통해 주입하였다.
    - form.html를 수정하여 post방식으로 게시글 작성 요청을 보내도록 수정하였다.

--- src/main/java/com/kakao/cafe/controller/ArticleController.java
+++ src/main/java/com/kakao/cafe/controller/ArticleController.java
@@ -1,13 +1,29 @@
 package com.kakao.cafe.controller;
 
+import com.kakao.cafe.domain.Article;
+import com.kakao.cafe.service.ArticleService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ArticleController {
+
+    private ArticleService articleService;
+
+    public ArticleController(ArticleService articleService) {
+        this.articleService = articleService;
+    }
 
     @GetMapping("/qna/form")
     public String form() {
         return "qna/form";
     }
+
+    @PostMapping("/qna/form")
+    public String create(Article article) {
+        articleService.write(article);
+        return "redirect:/";
+    }
+
 }
