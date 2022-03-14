8c00429785437e3b7e921a095030828598acaedd
Kang Changgu (Miller) <rkckr1@naver.com>
2022-03-14 18:06:23 +0900

    feat: session 정보 없이 지정된 path 에 접근할 수 없도록 HandlerInterceptor 추가
    - HandlerInterceptor 인터페이스를 구현한 AuthInterceptor 클래스 구현
    - AuthInterceptor 클래스 내 preHandle 메서드 오버라이딩으로 세션 내 SESSION_USER 키에 유저의 세선 정보가 있는지 확인
    - README.md 의 기능 요구사항 체크

---
+++ src/main/java/com/kakao/cafe/controller/AuthInterceptor.java
@@ -0,0 +1,37 @@
+package com.kakao.cafe.controller;
+
+import com.kakao.cafe.dto.UserResponse;
+import com.kakao.cafe.util.SessionUtil;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Component
+public class AuthInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response,
+        Object handler) throws Exception {
+
+        if (!(handler instanceof HandlerMethod)) {
+            return true;
+        }
+
+        HttpSession session = request.getSession();
+        if (session == null) {
+            response.sendRedirect("/error");
+            return false;
+        }
+
+        UserResponse user = (UserResponse) session.getAttribute(SessionUtil.SESSION_USER);
+        if (user == null) {
+            response.sendRedirect("/error");
+            return false;
+        }
+        return true;
+    }
+
+}
