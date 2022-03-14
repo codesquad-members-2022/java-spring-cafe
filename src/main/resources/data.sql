ad54ecb35fecc7c9fa7c21ec69f12e1a2a86e8f3
“kukim” <kukim.dev@gmail.com>
2022-03-14 22:44:40 +0900

    feat: User와 Article 테이블 생성과 더미값 sql 구현, 해당 sql H2 실행 시 init 하도록 설정
    - application.properties의 spring.sql.init.mode=always를 통해 resources에 있는 schema.sql, data.sql 초기 실행되도록 옵션 설정
    - schema.sql에서 User, Article 테이블 생성 (DDL)
    - data.sql에서 User, Article 더미 데이터 추가 (DML)

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO USER VALUES ('bob','1234','bob1','bob@gmail.com');
+INSERT INTO USER VALUES ('jona','1234','jona1','jona@gmail.com');
+INSERT INTO USER VALUES ('kim','1234','kim1','kim@gmail.com');
+INSERT INTO USER VALUES ('lee','1234','lee1','lee@gmail.com');
+
+INSERT INTO ARTICLE (writer, title, contents, created_time) VALUES ('bob', 'title1', 'content1', '2022-03-10 01:01');
+INSERT INTO ARTICLE (writer, title, contents, created_time) VALUES ('jona', 'title2', 'content2', '2022-03-10 02:22');
+INSERT INTO ARTICLE (writer, title, contents, created_time) VALUES ('kim', 'title3', 'content3', '2022-03-10 03:33');
+INSERT INTO ARTICLE (writer, title, contents, created_time) VALUES ('kim', 'title4', 'content4', '2022-03-10 04:44');
