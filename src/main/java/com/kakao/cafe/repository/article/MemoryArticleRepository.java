2a33826c2e621d99c62263ff50583c72822404af
donggi-lee-bit <devdonggilee@gmail.com>
2022-03-14 19:26:44 +0900

    게시글을 저장하는 저장소 기능 추가
    - 유저 저장소에서 사용하던 메서드를 활용하여 게시글을 저장하도록 하였습니다.

---
+++ src/main/java/com/kakao/cafe/repository/article/MemoryArticleRepository.java
@@ -0,0 +1,37 @@
+package com.kakao.cafe.repository.article;
+
+import com.kakao.cafe.domain.Article;
+import com.kakao.cafe.repository.CafeRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+@Repository
+public class MemoryArticleRepository implements CafeRepository<Article, Integer> {
+
+    private List<Article> articles = new CopyOnWriteArrayList<>();
+
+    @Override
+    public void save(Article article) {
+        articles.add(article);
+    }
+
+    @Override
+    public Optional<Article> findByName(Integer id) {
+        return articles.stream()
+                .filter(article -> article.getId() == id)
+                .findAny();
+    }
+
+    @Override
+    public List<Article> findAll() {
+        return new ArrayList<>(List.copyOf(articles));
+    }
+
+    public void clearStore() {
+        articles.clear();
+    }
+}
