20e93afb94a990d3e19e649572d09c9bb7c542ec
rkolx <olx.rko.o@gmail.com>
2022-03-15 01:45:16 +0900

    feat : ArticleController 클래스 생성 및 HomeController 수정
    - ArticleController 클래스
      - GetMapping("/questions") : 질문등록 페이지 반환
      - PostMapping("/questions") : 게시글 생성 후 "/"로 반환
      - GetMapping("/{id}") : id와 일치하는 게시글 상세보기
    - HomeController 클래스
      - GetMapping("/") : 기본 페이지, 등록한 게시글 리스트가 보여짐

---
+++ src/main/java/com/kakao/cafe/controller/ArticleController.java
@@ -0,0 +1,39 @@
+package com.kakao.cafe.controller;
+
+import com.kakao.cafe.domain.Article;
+import com.kakao.cafe.servcie.ArticleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class ArticleController {
+
+    private final ArticleService articleService;
+
+    @Autowired
+    public ArticleController(ArticleService articleService) {
+        this.articleService = articleService;
+    }
+
+    @GetMapping("/questions")
+    public String qnaWriteForm(){
+        return "post/form";
+    }
+
+    @PostMapping("/questions")
+    public String qnaWriteRegister(@ModelAttribute Article article){
+        articleService.qnaWrite(article);
+        return "redirect:/";
+    }
+
+    @GetMapping("/{id}")
+    public String showWrite(@PathVariable int id, Model model){
+        model.addAttribute("article", articleService.findById(id));
+        return "post/show";
+    }
+}
