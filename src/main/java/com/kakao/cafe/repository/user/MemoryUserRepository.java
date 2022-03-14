fdd27ab3b63031ffd6c51e2fcfcd9f081dc5aa99
donggi-lee-bit <devdonggilee@gmail.com>
2022-03-14 19:27:56 +0900

    회원을 저장하는 클래스 인터페이스 변경
    - 제네릭스를 이용한 인터페이스로 변경하였습니다.

--- src/main/java/com/kakao/cafe/repository/user/MemoryUserRepository.java
+++ src/main/java/com/kakao/cafe/repository/user/MemoryUserRepository.java
@@ -1,6 +1,7 @@
-package com.kakao.cafe.repository;
+package com.kakao.cafe.repository.user;
 
 import com.kakao.cafe.domain.User;
+import com.kakao.cafe.repository.CafeRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
@@ -9,7 +10,7 @@ import java.util.Optional;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 @Repository
-public class MemoryUserRepository implements UserRepository{
+public class MemoryUserRepository implements CafeRepository<User, String> {
 
     private List<User> store = new CopyOnWriteArrayList<>();
 
