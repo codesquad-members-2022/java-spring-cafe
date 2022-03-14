1eb9ec9ebe8cced0f28f8fcbd87acd31235ee242
leekm0310 <leekm310@gmail.com>
2022-03-14 17:55:01 +0900

    feat: 글목록 조회 기능 구현
    * 메인 페이지에서 글 목록을 보여주기 때문에 메인 페이지가 매핑되어 있는 HomeController를 수정해 글 목록을 보여줄 수 있도록 했습니다.
    * 뒤늦게 글 작성일을 불러올 수 없다는 것을 깨닫고 Article에 getter를 추가했습니다.
    저번에 getter를 쓰지 않는 방법을 학습하라고 하셔서 일단 구현한 뒤 리팩토링 해볼 계획입니다.

--- src/main/java/com/kakao/cafe/controller/HomeController.java
+++ src/main/java/com/kakao/cafe/controller/HomeController.java
@@ -1,13 +1,26 @@
 package com.kakao.cafe.controller;
 
+import com.kakao.cafe.domain.Article;
+import com.kakao.cafe.service.ArticleService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
 
+    private final ArticleService articleService;
+
+    public HomeController(ArticleService articleService) {
+        this.articleService = articleService;
+    }
+
     @GetMapping("/")
-    public String home(){
+    public String home(Model model){
+        List<Article> articles = articleService.loadAllArticles();
+        model.addAttribute("articles", articles);
         return "index";
     }
 }
--- src/main/java/com/kakao/cafe/domain/Article.java
+++ src/main/java/com/kakao/cafe/domain/Article.java
@@ -27,4 +27,7 @@ public class Article {
         return content;
     }
 
+    public LocalDateTime getCreatedDate() {
+        return createdDate;
+    }
 }
