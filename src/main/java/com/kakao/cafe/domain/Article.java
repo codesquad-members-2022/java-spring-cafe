c2ea05f53001f85bfa73c4a5837fdba5fb132ed7
nak253 <nwon4413@naver.com>
2022-03-14 21:40:01 +0900

    게시글 정보를 저장할 Article 클래스를 작성하였다.
    - 사용자가 전달한 값을 객체에 담기위한 setter를 정의하였다.

---
+++ src/main/java/com/kakao/cafe/domain/Article.java
@@ -0,0 +1,21 @@
+package com.kakao.cafe.domain;
+
+public class Article {
+    private String writer;
+    private String title;
+    private String contents;
+
+
+    public void setWriter(String writer) {
+        this.writer = writer;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setContents(String contents) {
+        this.contents = contents;
+    }
+
+}
