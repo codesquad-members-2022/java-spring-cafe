d389f863081c91220ca3f2f8a1bdf4fbadb48a4b
dukcode <duk9741@gmail.com>
2022-03-14 23:35:13 +0900

    Feat: User domain 추가

---
+++ src/test/java/com/kakao/cafe/domain/User.java
@@ -0,0 +1,48 @@
+package com.kakao.cafe.domain;
+
+public class User {
+    private Long id;
+    private String email;
+    private String userId;
+    private String password;
+
+    public User() {}
+
+    public User(String email, String userId, String password) {
+        this.email = email;
+        this.userId = userId;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
