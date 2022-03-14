641e50be4afd702125daba5191421aac1263995a
dukcode <duk9741@gmail.com>
2022-03-14 23:52:42 +0900

    Feat: 회원 가입 기능 구현
    - /users/new GET method mapping
    - /users/new POST method mapping

---
+++ src/main/java/com/kakao/cafe/controller/UserController.java
@@ -0,0 +1,38 @@
+package com.kakao.cafe.controller;
+
+import com.kakao.cafe.domain.User;
+import com.kakao.cafe.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/users")
+public class UserController {
+
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/new")
+    public String newForm() {
+        return "user/form";
+    }
+
+    @PostMapping("/new")
+    public String signUp(@ModelAttribute User user) {
+        log.info("user.getNickName={}", user.getUserId());
+        userService.join(user);
+        return "redirect:/users";
+    }
+}
