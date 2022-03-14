2eaeca7705033bfec213147ea35666193758640d
who-hoo <who.ho3ov@gmail.com>
2022-03-15 03:10:43 +0900

    :white_check_mark: 테스트 코드 DisplayName 추가

--- src/test/java/com/kakao/cafe/repository/MemoryArticleRepositoryTest.java
+++ src/test/java/com/kakao/cafe/repository/MemoryArticleRepositoryTest.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.*;
 import com.kakao.cafe.domain.Article;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class MemoryArticleRepositoryTest {
@@ -17,6 +18,7 @@ class MemoryArticleRepositoryTest {
     }
 
     @Test
+    @DisplayName("게시글을 저장하면, 게시글을 ArrayList에 저장하고 id를 리턴한다.")
     void save() {
         //given
         Article validArticle = new Article("Anonymous", "test title", "test content");
@@ -29,6 +31,7 @@ class MemoryArticleRepositoryTest {
     }
 
     @Test
+    @DisplayName("게시글을 id로 조회하면, 해당 id를 가진 게시글을 리턴한다.")
     void findById() {
         //given
         Article givenArticle = new Article("Anonymous", "test title", "test content");
@@ -42,6 +45,7 @@ class MemoryArticleRepositoryTest {
     }
 
     @Test
+    @DisplayName("게시글을 전부 조회하면, 저장된 게시글 전부를 List에 담아 리턴한다.")
     void findAll() {
         //given
         for (int i = 1; i <= 10; i++) {
--- src/test/java/com/kakao/cafe/service/ArticleServiceTest.java
+++ src/test/java/com/kakao/cafe/service/ArticleServiceTest.java
@@ -10,6 +10,7 @@ import java.util.List;
 import java.util.NoSuchElementException;
 import org.assertj.core.api.ThrowableAssert.ThrowingCallable;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class ArticleServiceTest {
@@ -23,6 +24,7 @@ class ArticleServiceTest {
     }
 
     @Test
+    @DisplayName("게시글을 저장하면, 게시글을 ArrayList에 저장하고 id를 리턴한다.")
     void posting() {
         //given
         PostingRequestDto validPostingRequestDto =
@@ -36,6 +38,7 @@ class ArticleServiceTest {
     }
 
     @Test
+    @DisplayName("게시글을 존재하는 id로 조회하면, 해당 id를 가진 게시글의 조회수를 1 올려주고 해당 게시글을 리턴한다.")
     void valid_read() {
         //given
         Article givenArticle = new Article("Anonymous", "test title", "test content");
@@ -49,6 +52,7 @@ class ArticleServiceTest {
     }
 
     @Test
+    @DisplayName("게시글을 존재하지 않는 id로 조회하면, \"해당 아이디를 가진 게시글이 존재하지 않습니다.\"라는 NoSuchElementException을 던진다.")
     void invalid_read() {
         //given
         int givenId = -1;
@@ -63,6 +67,7 @@ class ArticleServiceTest {
     }
 
     @Test
+    @DisplayName("게시글을 전부 조회하면, 저장된 게시글 전부를 List에 담아 리턴한다.")
     void findPosts() {
         //given
         for (int i = 1; i <= 10; i++) {
