6325e998c5cd676cc412b6e337c868dd4f690b12
donggi-lee-bit <devdonggilee@gmail.com>
2022-03-14 19:30:43 +0900

    게시글 관련 사용자 입력을 받아서 저장한 Article 객체 생성
    - 사용자 입력을 받은 게시글 제목, 본문, 게시글 번호를 가질 Article 객체를 생성하였습니다.

--- src/main/java/com/kakao/cafe/domain/Article.java
+++ src/main/java/com/kakao/cafe/domain/Article.java
@@ -2,11 +2,17 @@ package com.kakao.cafe.domain;
 
 public class Article {
 
+    private int id;
     private String title;
     private String text;
 
-    public Article(String title, String text) {
+    public Article(int id, String title, String text) {
+        this.id = id;
         this.title = title;
         this.text = text;
+    }
+
+    public int getId() {
+        return id;
     }
 }
