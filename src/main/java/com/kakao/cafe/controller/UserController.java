874d205d558083faa215fb7077941acb328c5c67
who-hoo <who.ho3ov@gmail.com>
2022-03-15 03:01:51 +0900

    :sparkles: 회원 정보 수정 POST -> PUT 으로 변경

--- src/main/java/com/kakao/cafe/controller/UserController.java
+++ src/main/java/com/kakao/cafe/controller/UserController.java
@@ -10,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -50,7 +51,7 @@ public class UserController {
         return "user/updateForm";
     }
 
-    @PostMapping("/{userId}/update")
+    @PutMapping("/{userId}/update")
     public String updateUser(UserUpdateRequestDto form) {
         userService.updateUser(form);
         return "redirect:/users";
