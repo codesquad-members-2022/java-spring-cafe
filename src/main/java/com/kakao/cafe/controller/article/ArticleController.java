55a362ba9130dae93d87db3f84e76e729c6a1923
donggi-lee-bit <devdonggilee@gmail.com>
2022-03-15 16:38:13 +0900

    Controller에 상세 페이지 접근 메서드 추가
    - 상세 페이지 접근 메서드를 추가하고 URL은 /articles/{id}로 매핑하였습니다.

--- src/main/java/com/kakao/cafe/controller/article/ArticleController.java
+++ src/main/java/com/kakao/cafe/controller/article/ArticleController.java
@@ -6,9 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.annotation.PostConstruct;
 import java.util.List;
 
 @Controller
@@ -39,5 +41,19 @@ public class ArticleController {
         List<Article> articles = articleService.findArticles();
         model.addAttribute("articles", articles);
         return "home";
+    }
+
+    @GetMapping("/{id}")
+    public String article(@PathVariable Integer id, Model model) {
+        Article article = articleService.findArticle(id);
+        model.addAttribute("article", article);
+        return "/articles/article";
+    }
+
+    @PostConstruct
+    public void init() {
+
+        articleService.join(new Article(0, "코드스쿼드 문 열려면?", "example1"));
+        articleService.join(new Article(1, "문지기가 필요합니다. 문지기 할 사람?", "example2"));
     }
 }
