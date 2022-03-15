467147fd380854c3c7a06fd3a307edf00a257d00
donggi-lee-bit <devdonggilee@gmail.com>
2022-03-15 12:41:51 +0900

    View에서 Model을 통해 전달한 게시글 목록을 출력하도록 Controller 추가
    - 모든 게시글을 가져와서 Model에 담아 View에 전달하였습니다.

--- src/main/java/com/kakao/cafe/controller/article/ArticleController.java
+++ src/main/java/com/kakao/cafe/controller/article/ArticleController.java
@@ -4,9 +4,12 @@ import com.kakao.cafe.domain.Article;
 import com.kakao.cafe.service.ArticleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/articles")
@@ -29,5 +32,12 @@ public class ArticleController {
         Article article = new Article(form.getId(), form.getTitle(), form.getText());
         articleService.join(article);
         return "redirect:/";
+    }
+
+    @GetMapping
+    public String articles(Model model) {
+        List<Article> articles = articleService.findArticles();
+        model.addAttribute("articles", articles);
+        return "home";
     }
 }
