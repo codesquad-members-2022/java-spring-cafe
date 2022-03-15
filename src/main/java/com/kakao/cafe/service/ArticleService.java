eb69c738a2875b2222be4a2992bc946101776619
seyeong <seyoung7555@naver.com>
2022-03-16 01:23:49 +0900

    feat(ArticleService): 게시글 수정을 위한 서비스 계층 로직 설정

--- src/main/java/com/kakao/cafe/service/ArticleService.java
+++ src/main/java/com/kakao/cafe/service/ArticleService.java
@@ -1,6 +1,7 @@
 package com.kakao.cafe.service;
 
 import com.kakao.cafe.controller.dto.ArticleSaveRequestDto;
+import com.kakao.cafe.controller.dto.ArticleUpdateRequestDto;
 import com.kakao.cafe.domain.article.Article;
 import com.kakao.cafe.domain.article.ArticleRepository;
 import org.springframework.stereotype.Service;
@@ -26,5 +27,15 @@ public class ArticleService {
     public Article findById(Long id) {
         return articleRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("해당 글이 존재하지 않습니다."));
+    }
+
+    public void update(ArticleUpdateRequestDto dto) {
+        Long id = dto.getId();
+        String title = dto.getTitle();
+        String contents = dto.getContents();
+
+        Article article = findById(id);
+        Article updateArticle = article.update(title, contents);
+        articleRepository.save(updateArticle);
     }
 }
