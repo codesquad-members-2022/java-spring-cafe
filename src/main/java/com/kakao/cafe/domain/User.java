741f98b34d4730458b47ef6de2417bc01568ac71
ByeoungJoon Jeon <juni8453@naver.com>
2022-03-15 21:02:20 +0900

    수정) User 도메인 객체 매개변수가 있는 생성자 제거
    원래 userController createUser() 메서드에서 User 도메인 객체를 생성하며 @ModelAttribute UserCreateDto
    
    의 프로퍼티 값들을 User 객체의 생성자 값으로 넣어줬었습니다. 하지만 아직 Dto 는 필요없다고 판단해서 UserCreateDto
    
    객체를 지우고, User 객체의 생성자를 직접 Controller 에서 사용할 일이 없기 떄문에 생성자를 지웠습니다.

--- src/main/java/com/kakao/cafe/domain/User.java
+++ src/main/java/com/kakao/cafe/domain/User.java
@@ -3,25 +3,35 @@ package com.kakao.cafe.domain;
 public class User {
 
     private long userIdx;
-    private final String userName;
-    private final String userPassword;
-    private final String userEmail;
-
-    public User(String userName, String userPassword, String userEmail) {
-        this.userName = userName;
-        this.userPassword = userPassword;
-        this.userEmail = userEmail;
-    }
+    private String userName;
+    private String userPassword;
+    private String userEmail;
 
     public long getUserIdx() {
         return userIdx;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
     }
 
     public void setUserIdx(long userIdx) {
         this.userIdx = userIdx;
     }
 
-    public String getUserName() {
-        return userName;
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
     }
 }
