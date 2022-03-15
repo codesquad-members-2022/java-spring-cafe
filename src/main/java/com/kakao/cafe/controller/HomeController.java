54bb9fb7351e28500f822a0553cf4ca785d023ef
hwicode <khc3796@naver.com>
2022-03-15 11:46:19 +0900

    홈 화면에서 유저의 리스트가 보이도록 HomeController 수정
    (cherry picked from commit 8c0a92a6c7a64924229d4fc4de1e3d6457220ccc)

--- src/main/java/com/kakao/cafe/controller/HomeController.java
+++ src/main/java/com/kakao/cafe/controller/HomeController.java
@@ -1,11 +1,22 @@
 package com.kakao.cafe.controller;
 
+import com.kakao.cafe.service.ArticleService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class HomeController {
 
+    private final ArticleService articleService;
+
+    public HomeController(ArticleService articleService) {
+        this.articleService = articleService;
+    }
+
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+        model.addAttribute("articles", articleService.findArticles());
         return "index";
     }
 }
