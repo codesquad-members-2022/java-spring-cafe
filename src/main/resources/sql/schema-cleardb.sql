1da2f65ee37b500c64ab1ce5c5ce819191ecbe9e
jinan159 <wlsdhks0423@naver.com>
2022-03-15 15:47:53 +0900

    build: heroku cleardb 용 스키마 쿼리 추가

---
+++ src/main/resources/sql/schema-cleardb.sql
@@ -0,0 +1,22 @@
+DROP TABLE IF EXISTS `cafe_users`;
+
+CREATE TABLE `cafe_users` (
+  id BIGINT AUTO_INCREMENT PRIMARY KEY,
+  userId VARCHAR(20) NOT NULL UNIQUE,
+  passwd CHAR(41) NOT NULL,
+  name VARCHAR(40) NOT NULL,
+  email VARCHAR(320) NOT NULL,
+  created_date DATETIME NOT NULL,
+  modified_date DATETIME NOT NULL
+);
+
+DROP TABLE IF EXISTS `article`;
+
+CREATE TABLE `article` (
+   id BIGINT AUTO_INCREMENT PRIMARY KEY,
+   title VARCHAR(100) NOT NULL,
+   content TEXT,
+   view_count BIGINT DEFAULT 0,
+   created_date DATETIME NOT NULL,
+   modified_date DATETIME NOT NULL
+);
