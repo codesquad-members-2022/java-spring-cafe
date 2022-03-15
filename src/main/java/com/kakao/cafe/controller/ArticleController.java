e4bbffce8626ad698e9a8b53ddce395ac66db34e
ByeoungJoon Jeon <juni8453@naver.com>
2022-03-15 12:00:07 +0900

    수정) ArticleController 중복 URL 제거 및 @Autowired 제거
    ArticleController 에는 생성자가 하나이기 때문에 명시를 하고 싶어서 놔두었던 @Autowired 어노테이션을 삭제했습니다.
    
    @RequestMappint 어노테이션을 클래스 레벨에 적용해 중복된 URL 을 모두 공통으로 뽑아냈습니다.
    
    수정 전 UserController 와는 다르게 모든 URL 시작이 "/article/ ~ " 이였기 때문에 순조롭게 공통 URL 을 제거할 수 있었습니다.

--- src/main/java/com/kakao/cafe/controller/ArticleController.java
+++ src/main/java/com/kakao/cafe/controller/ArticleController.java
@@ -2,37 +2,38 @@ package com.kakao.cafe.controller;
 
 import com.kakao.cafe.domain.Article;
 import com.kakao.cafe.service.ArticleService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 import java.util.List;
 
+@RequestMapping("/article")
 @Controller
 public class ArticleController {
 
     private final ArticleService articleService;
 
-    @Autowired
     public ArticleController(ArticleService articleService) {
         this.articleService = articleService;
     }
 
-    @GetMapping("/article/questions")
+    @GetMapping("/questions")
     public String createArticleForm() {
         return "/article/questions";
     }
 
-    @PostMapping("/article/questions")
+    @PostMapping("/questions")
     public String crateArticle(Article article) {
         articleService.createArticle(article);
 
         return "redirect:/";
     }
 
-    @GetMapping("/article/list")
+    @GetMapping("/list")
     public String articleList(Model model) {
         List<Article> articles = articleService.findAllArticle();
         model.addAttribute("articles", articles);
@@ -40,7 +41,7 @@ public class ArticleController {
         return "/article/list";
     }
 
-    @GetMapping("/article/{articleIdx}")
+    @GetMapping("/{articleIdx}")
     public String articleDetail(@PathVariable long articleIdx, Model model) {
         Article foundArticle = articleService.findArticle(articleIdx);
         model.addAttribute("foundArticle", foundArticle);
