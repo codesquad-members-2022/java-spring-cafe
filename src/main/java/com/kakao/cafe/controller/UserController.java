60068b1e6c1c1cf446def6fc6555abd2701ada14
ByeoungJoon Jeon <juni8453@naver.com>
2022-03-15 11:41:20 +0900

    수정) UserController 중복 URL 제거에 따른 HomeController 추가 및 @Autowired 제거
    UserController 에는 생성자가 하나이기 때문에 명시를 하고 싶어서 놔두었던 @Autowired 어노테이션을 삭제했습니다.
    
    @RequestMappint 어노테이션을 클래스 레벨에 적용해 중복된 URL 을 모두 공통으로 뽑아냈습니다. 이때 홈페이지가
    
    실행되는 메서드가 UserController 에 있어서, 따로 HomeController 를 추가해 해당 컨트롤러에서 홈페이지가
    
    실행되도록 변경했습니다.

--- src/main/java/com/kakao/cafe/controller/UserController.java
+++ src/main/java/com/kakao/cafe/controller/UserController.java
@@ -2,42 +2,37 @@ package com.kakao.cafe.controller;
 
 import com.kakao.cafe.domain.User;
 import com.kakao.cafe.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import java.util.List;
 
+@RequestMapping("/user")
 @Controller
 public class UserController {
 
     private final UserService userService;
 
-    @Autowired
     UserController(UserService userService) {
         this.userService = userService;
     }
 
-    @GetMapping("/")
-    public String homePage() {
-        return "/home/index";
-    }
-
-    @GetMapping("/user/create")
+    @GetMapping("/create")
     public String createForm() {
         return "/user/create";
     }
 
-    @PostMapping("/user/create")
+    @PostMapping("/create")
     public String createUser(User user) {
         userService.createUser(user);
 
         return "redirect:/user/list";
     }
 
-    @GetMapping("/user/list")
+    @GetMapping("/list")
     public String userList(Model model) {
         List<User> users = userService.findAllUsers();
         model.addAttribute("users", users);
@@ -45,7 +40,7 @@ public class UserController {
         return "/user/list";
     }
 
-    @GetMapping("/user/{userName}")
+    @GetMapping("/{userName}")
     public String userProfile(@PathVariable String userName, Model model) {
         User foundUser = userService.findUser(userName);
         model.addAttribute("foundUser", foundUser);
