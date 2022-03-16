254d2bdbeeba16c4d8bb241cede67b1adff5f298
hsjang <hsjang0405@gmail.com>
2022-03-16 11:27:47 +0900

    🔥  id, password 확인차 적었던 logger 제거

--- src/main/java/com/kakao/cafe/web/controller/LoginController.java
+++ src/main/java/com/kakao/cafe/web/controller/LoginController.java
@@ -3,19 +3,14 @@ package com.kakao.cafe.web.controller;
 import com.kakao.cafe.web.domain.user.User;
 import com.kakao.cafe.web.service.LoginService;
 import javax.servlet.http.HttpSession;
-import org.apache.juli.logging.Log;
-import org.apache.juli.logging.LogFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/user/login")
 public class LoginController {
-
-	private final Log log = LogFactory.getLog(getClass());
 
 	private final LoginService loginService;
 
@@ -29,22 +24,14 @@ public class LoginController {
 	}
 
 	@PostMapping
-	public String login(@RequestParam String userId, @RequestParam String password, HttpSession session) {
-		log.info(userId);
-		log.info(password);
-		log.info(session);
-
-		User loginUser = loginService.login(userId, password);
+	public String login(LoginForm form, HttpSession session) {
+		User loginUser = loginService.login(form.getUserId(), form.getPassword());
 
 		if (loginUser == null) {
-			log.info("잘못된 로그인 요청");
-			return "redirect:/user/login";
+			return "redirect:/user/login_failed";
 		}
 
-		log.info("로그인 성공");
 		session.setAttribute("loginUser", loginUser);
-
-		log.info(session.getAttribute("loginUser").toString());
 
 		return "redirect:/";
 	}
