885e51f581463958a7527794374508e51a44e14f
naneun <rmtlrksdp@naver.com>
2022-03-17 06:38:46 +0900

    🐛 AuthoInterceptor 버그 수정
    - preHandle 메서드에서 /articles/{id}/update, /articles/{id}remove uri 에 대한 권한을 검사하도록 수정한다.
    - 두 메서드 모두 writer (작성자) 와 session userInfo 값 (로그인한 사용자) 의 userId 를 비교하여 해당 글에 대한 권한이 있는 지 검사한다.
    - PostHandle 메서드의 경우 /articles/{id}/form uri 만 담당한다. 불러 오려는 글 수정 폼 페이지가 자신이 작성한 글에 대한 페이지가 아니라면 예외 페이지로 이동한다.

--- src/main/java/com/kakao/cafe/config/MvcConfig.java
+++ src/main/java/com/kakao/cafe/config/MvcConfig.java
@@ -23,6 +23,7 @@ public class MvcConfig implements WebMvcConfigurer {
 
         registry.addViewController("/articles/form").setViewName("qna/form");
 
+        registry.addViewController("/error/401").setViewName("error/401");
         registry.addViewController("/error/403").setViewName("error/403");
     }
 
@@ -37,10 +38,7 @@ public class MvcConfig implements WebMvcConfigurer {
                 .addPathPatterns(
                         "/articles/form",
                         "/articles/write",
-                        "/articles/{id}",
-                        "/articles/{id}/form",
-                        "/articles/{id}/update",
-                        "/articles/{id}/remove"
+                        "/articles/{id}/*"
                 );
     }
 }
--- src/main/java/com/kakao/cafe/interceptor/AuthInterceptor.java
+++ src/main/java/com/kakao/cafe/interceptor/AuthInterceptor.java
@@ -3,6 +3,8 @@ package com.kakao.cafe.interceptor;
 import com.kakao.cafe.domain.Article;
 import com.kakao.cafe.domain.User;
 import org.apache.logging.log4j.util.Strings;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -10,19 +12,36 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.regex.Pattern;
 
 public class AuthInterceptor implements HandlerInterceptor {
 
+    private static final Logger log = LoggerFactory.getLogger(AuthInterceptor.class);
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response,
                              Object handler) throws IOException {
+
+        debug(request);
 
         HttpSession session = request.getSession();
         User user = (User) session.getAttribute("userInfo");
 
         if (user == null) {
             response.sendRedirect("/users/login");
+            return false;
+        }
+
+        String writer = request.getParameter("writer");
+        String currentURI = request.getRequestURI();
+
+        if (Strings.isNotBlank(writer) && !writer.equals(user.getUserId())
+                && (Pattern.matches("^/articles/[0-9]+/update$", currentURI)
+                || Pattern.matches("^/articles/[0-9]+/remove$", currentURI))) {
+
+            response.sendRedirect("/error/403");
             return false;
         }
 
@@ -33,19 +52,29 @@ public class AuthInterceptor implements HandlerInterceptor {
     public void postHandle(HttpServletRequest request, HttpServletResponse response,
                            Object handler, ModelAndView modelAndView) throws IOException {
 
+        String currentURI = request.getRequestURI();
+        if (!Pattern.matches("^/articles/[0-9]+/form$", currentURI)) {
+            return;
+        }
+
+        debug(request);
+
         HttpSession session = request.getSession();
         User user = (User) session.getAttribute("userInfo");
-
         Article article = (Article) modelAndView.getModel().get("article");
-        String writer = article.getWriter();
-        String currentURI = request.getRequestURI();
-        if (Strings.isNotBlank(writer)
-                && !writer.equals(user.getUserId())
-                && Pattern.matches("^/articles/[0-9]+/form$", currentURI)
-                || Pattern.matches("^/articles/[0-9]+/update$", currentURI)
-                || Pattern.matches("^/articles/[0-9]+/remove$", currentURI)) {
 
+        String writer = article.getWriter();
+
+        if (Strings.isNotBlank(writer) && !writer.equals(user.getUserId())) {
             response.sendRedirect("/error/403");
         }
+    }
+
+    private void debug(HttpServletRequest request) {
+        Map<String, Object> params = new HashMap<>();
+        request.getParameterNames().asIterator()
+                .forEachRemaining(name -> params.put(name, request.getParameter(name)));
+
+        log.debug("AuthInterceptor: {} {} {}", request.getMethod(), request.getRequestURI(), params);
     }
 }
