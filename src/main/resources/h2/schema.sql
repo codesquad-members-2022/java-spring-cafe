3a81e52eba57b039bbfacc380db2024f9b385ac8
Louie <dhdustnr0134@naver.com>
2022-03-16 20:37:13 +0900

    ARTICLE 테이블 스키마, 샘플 데이터 추가

--- src/main/resources/h2/schema.sql
+++ src/main/resources/h2/schema.sql
@@ -1,9 +1,18 @@
-DROP TABLE user IF EXISTS;
+DROP TABLE ARTICLE IF EXISTS;
+DROP TABLE USER IF EXISTS;
 
-create table user (
-    userId varchar(100),
-    password varchar(100) not null,
-    name varchar(100) not null,
-    email varchar(100),
-    primary key (userId)
+create table USER (
+    USER_ID VARCHAR(100) not null primary key,
+    PASSWORD VARCHAR(100) not null,
+    NAME VARCHAR(100) not null,
+    EMAIL VARCHAR(100)
+);
+
+create table ARTICLE (
+    ID BIGINT not null AUTO_INCREMENT primary key,
+    USER_ID VARCHAR(100),
+    CONTENTS VARCHAR(1000),
+    TITLE VARCHAR(100),
+    CREATE_DATE DATETIME default CURRENT_TIMESTAMP,
+    foreign key (USER_ID) references USER(USER_ID)
 );
