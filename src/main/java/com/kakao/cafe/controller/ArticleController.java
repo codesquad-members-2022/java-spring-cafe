6d4bac0de91135478379a52b34dc9cd4d09dad86
seyeong <seyoung7555@naver.com>
2022-03-16 10:58:14 +0900

    feat(ArticleController): 컨트롤러에 삭제 기능 매핑

--- src/main/java/com/kakao/cafe/controller/ArticleController.java
+++ src/main/java/com/kakao/cafe/controller/ArticleController.java
@@ -7,10 +7,7 @@ import com.kakao.cafe.domain.user.User;
 import com.kakao.cafe.service.ArticleService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
 
@@ -41,25 +38,33 @@ public class ArticleController {
         return "qna/show";
     }
 
+    @DeleteMapping("/articles/{id}")
+    public String delete(@PathVariable Long id, HttpSession session) {
+        validateSessionUser(id, session);
+        articleService.deleteById(id);
+        return "redirect:/";
+    }
+
     @GetMapping("/articles/{id}/update")
     public String updateForm(@PathVariable Long id, Model model, HttpSession session) {
+        validateSessionUser(id, session);
         Article article = articleService.findById(id);
-        validateSessionUser(article.getWriter(), session);
         model.addAttribute("article", article);
         return "qna/update_form";
     }
 
     @PutMapping("/articles/{id}/update")
     public String update(@PathVariable Long id, ArticleUpdateRequestDto dto, HttpSession session) {
-        Article article = articleService.findById(id);
-        validateSessionUser(article.getWriter(), session);
+        validateSessionUser(id, session);
         articleService.update(dto);
         return "redirect:/articles/" + id;
     }
 
-    private void validateSessionUser(String userId, HttpSession session) {
+    private void validateSessionUser(Long id, HttpSession session) {
+        Article article = articleService.findById(id);
+        String writer = article.getWriter();
         Object sessionUser = session.getAttribute("sessionUser");
-        if (sessionUser == null || !((User) sessionUser).getUserId().equals(userId)) {
+        if (sessionUser == null || !((User) sessionUser).getUserId().equals(writer)) {
             throw new IllegalArgumentException("본인이 작성한 글만 수정 가능합니다");
         }
     }
