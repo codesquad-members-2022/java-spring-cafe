7fc72ab4982ef085cfffee9ee4d5ae3d6def84f8
Kang Changgu (Miller) <rkckr1@naver.com>
2022-03-16 15:41:09 +0900

    feat: ajax 를 이용한 댓글 삭제 기능 구현
    - /resources/templates/qna/show.html 에 deleteAnswer 함수 추가
    - /resources/templates/components/answerTemplate.html 내 경로 수정
    - 댓글 추가/삭제 시 개수 증가/감소 표시
    - RestController 에서 사용할 Result VO 추가
    - /resources/query.yml 에서 SELECT_REPLIES 수정 (댓글 id 내림차순 정렬)

--- src/main/java/com/kakao/cafe/controller/ReplyController.java
+++ src/main/java/com/kakao/cafe/controller/ReplyController.java
@@ -1,10 +1,12 @@
 package com.kakao.cafe.controller;
 
 import com.kakao.cafe.dto.ReplyResponse;
+import com.kakao.cafe.dto.Result;
 import com.kakao.cafe.service.ReplyService;
 import com.kakao.cafe.session.SessionUser;
 import javax.servlet.http.HttpSession;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,11 +30,17 @@ public class ReplyController {
         return replyService.comment(user, articleId, comment);
     }
 
-    @DeleteMapping("/{questionId}/answers/{id}")
-    public String deleteAnswer(@PathVariable(value = "id") Integer replyId, HttpSession session) {
+    @DeleteMapping(value = "/{questionId}/answers/{id}")
+    public Result deleteAnswer(@PathVariable(value = "id") Integer replyId, HttpSession session) {
         SessionUser user = SessionUser.from(session);
         replyService.deleteReply(user, replyId);
-        return "redirect:/";
+        return Result.ok();
     }
+
+    @ExceptionHandler
+    public Result handleException(Exception e) {
+        return Result.error(e);
+    }
+
 
 }
