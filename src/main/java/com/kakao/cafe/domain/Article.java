4c8535c79f446f649198becf09d28dfdc38b8b50
Louie <dhdustnr0134@naver.com>
2022-03-17 02:28:10 +0900

    Article 조회용 생성자 추가

--- src/main/java/com/kakao/cafe/domain/Article.java
+++ src/main/java/com/kakao/cafe/domain/Article.java
@@ -8,12 +8,20 @@ public class Article {
     private User user;
     private String title;
     private List<String> contents;
-    private LocalDateTime createDate = LocalDateTime.now();
+    private LocalDateTime createDate;
 
+//    게시글을 등록할 때 사용하는 생성자
     public Article(User user, String title, List<String> contents) {
+        this(null, user, title, contents, LocalDateTime.now());
+    }
+
+//    게시글을 데이터베이스에서 조회할 때 사용하는 생성자
+    public Article(Integer id, User user, String title, List<String> contents, LocalDateTime createDate) {
+        this.id = id;
         this.user = user;
         this.title = title;
         this.contents = contents;
+        this.createDate = createDate;
     }
 
     public boolean isEqualsTitle(String title) {
