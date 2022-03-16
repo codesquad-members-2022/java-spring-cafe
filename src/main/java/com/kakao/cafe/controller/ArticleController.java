3196567457366eeadf100d63fa47af9e6a7e4d64
leejohy-0223 <leejohy@naver.com>
2022-03-16 15:56:20 +0900

    feat: ArticleController에서 세션 검증을 통해, 게시글 수정 여부를 체크한 후 ArticleService에게 update 요청을 보내도록 구현

--- src/main/java/com/kakao/cafe/controller/ArticleController.java
+++ src/main/java/com/kakao/cafe/controller/ArticleController.java
@@ -1,5 +1,8 @@
 package com.kakao.cafe.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -7,8 +10,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 import com.kakao.cafe.domain.article.Article;
+import com.kakao.cafe.domain.user.User;
 import com.kakao.cafe.service.ArticleService;
 
 @Controller
@@ -41,5 +46,39 @@ public class ArticleController {
         log.info("GET /articles/{}", index);
         model.addAttribute("article", articleService.showArticle(index));
         return "qna/showQna";
+    }
+
+    @GetMapping("/articles/{index}/edit")
+    public String editArticles(@PathVariable int index, HttpServletRequest request, Model model) {
+        log.info("GET /articles/{}/edit", index);
+        Article article = validateSessionUser(index, request);
+        model.addAttribute("article", article);
+        return "qna/editQna";
+    }
+
+    @PutMapping("/articles/{index}/edit")
+    public String updateArticles(@PathVariable int index, Article updateArticle, HttpServletRequest request) {
+        log.info("POST /articles/{}/edit", index);
+        findSessionUser(request.getSession());
+        articleService.update(index, updateArticle);
+        return "redirect:/";
+    }
+
+    private Article validateSessionUser(int index, HttpServletRequest request) {
+        HttpSession session = request.getSession();
+        User user = findSessionUser(session);
+        Article article = articleService.showArticle(index);
+        if (!article.isYourWriter(user.getUserId())) {
+            throw new IllegalArgumentException("[ERROR] 작성한 사용자만 수정할 수 있습니다.");
+        }
+        return article;
+    }
+
+    private User findSessionUser(HttpSession session) {
+        Object value = session.getAttribute("sessionUser");
+        if (value == null) {
+            throw new IllegalArgumentException("[ERROR] 로그인이 필요합니다.");
+        }
+        return (User) value;
     }
 }
