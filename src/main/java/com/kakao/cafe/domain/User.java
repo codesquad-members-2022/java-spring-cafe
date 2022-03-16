6bfc48378d311a62d199190df1ea25b1c9d383f0
rkolx <olx.rko.o@gmail.com>
2022-03-17 01:04:50 +0900

    update : 생성자 삭제

--- src/main/java/com/kakao/cafe/domain/User.java
+++ src/main/java/com/kakao/cafe/domain/User.java
@@ -1,21 +1,20 @@
 package com.kakao.cafe.domain;
 
+import java.time.LocalDateTime;
+
 public class User {
     private Long id;
     private String nickname;
     private String email;
     private String password;
+    private LocalDateTime createdDate;
 
-    public User(Long id, String nickname, String email, String password) {
+/*    public User(Long id, String nickname, String email, String password) {
         this.id = id;
         this.nickname = nickname;
         this.email = email;
         this.password = password;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
+    }*/
 
     public long getId() {
         return id;
@@ -31,5 +30,29 @@ public class User {
 
     public String getPassword() {
         return password;
+    }
+
+    public void setCreatedDate(LocalDateTime createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public LocalDateTime getCreatedDate() {
+        return createdDate;
     }
 }
