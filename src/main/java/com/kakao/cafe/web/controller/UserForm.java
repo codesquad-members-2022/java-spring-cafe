ed925ef629fa01fbf5db6a1d0e814bf6e31097d2
hsjang <hsjang0405@gmail.com>
2022-03-16 11:56:46 +0900

    🐛  생성자 없어서 값 못받아오는 오류 해결

--- src/main/java/com/kakao/cafe/web/controller/UserForm.java
+++ src/main/java/com/kakao/cafe/web/controller/UserForm.java
@@ -7,6 +7,13 @@ public class UserForm {
 	private String name;
 	private String email;
 
+	public UserForm(String userId, String password, String name, String email) {
+		this.userId = userId;
+		this.password = password;
+		this.name = name;
+		this.email = email;
+	}
+
 	public String getUserId() {
 		return userId;
 	}
