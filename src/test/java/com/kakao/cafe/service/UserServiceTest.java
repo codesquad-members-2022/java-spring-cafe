112d7e18f8affafb4d47b8a2833a68d4dfe2c462
함석호 <kalejuice02@gmail.com>
2022-03-18 01:09:58 +0900

    refactor : 서비스 테스트에 @SpringBootTest 어노테이션 추가

--- src/test/java/com/kakao/cafe/service/UserServiceTest.java
+++ src/test/java/com/kakao/cafe/service/UserServiceTest.java
@@ -8,17 +8,20 @@ import com.kakao.cafe.service.UserService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+@SpringBootTest
 public class UserServiceTest {
 
-    UserRepository repository = new UserMemoryRepository();
-    UserService service = new UserService(repository);
-
+    @Autowired
+    UserRepository repository;
+    @Autowired
+    UserService service;
     @Test
     @DisplayName("회원가입")
     void join() {
