138feaa740a8f80e4fb61316050144d20ad38555
jiwoo <zbqmgldjfh@gmail.com>
2022-03-17 16:12:03 +0900

    setting: 스프링 설정을 통해 매번 실행시 데이터 초기화 되도록 변경

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,10 +1,13 @@
 -- DML
 
-INSERT INTO article (user_id, title, contents, local_date_time)
-VALUES ('javajigi', 'title입니다', 'content입니다', now());
+INSERT INTO user_info (user_id, password, name, email)
+VALUES ('test1', '1234', 'test1', 'test1@gmail.com');
+
+INSERT INTO user_info (user_id, password, name, email)
+VALUES ('test2', '5678', 'test2', 'test2@gmail.com');
 
 INSERT INTO article (user_id, title, contents, local_date_time)
-VALUES ('test1', 'title입니다', 'content입니다', now());
+VALUES ('test1', 'test게시물 입니다', '맥북 너무 좋아~', now());
 
 INSERT INTO article (user_id, title, contents, local_date_time)
-VALUES ('test2', 'title입니다', 'content입니다', now());
+VALUES ('test2', 'Shine공주 입니다', '샤랄라 빛나는 중입니다!', now());
