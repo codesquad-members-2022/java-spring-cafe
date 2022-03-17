4a5ff9bf66e311cdeb34b5873d740c32b08dd716
geombong <shoy1415@gmail.com>
2022-03-18 00:19:10 +0900

    chore. html 파일 수정

--- src/main/resources/sql/schema.sql
+++ src/main/resources/sql/schema.sql
@@ -7,16 +7,16 @@ create table `user`
     `userId`   varchar(30) NOT NULL,
     `name`     varchar(30) NOT NULL,
     `password` varchar(30) NOT NULL,
-    `email`    varchar(30) NOT NULL
+    `email` varchar(30)
 );
 
 create table `article`
 (
-    `id`          BIGINT primary key auto_increment,
-    `title`       varchar(64)   NOT NULL,
-    `writer`      varchar(30)   NOT NULL,
-    `contents`    text          NOT NULL,
-    `dateOfIssue` smalldatetime NOT NULL
+    `id`       BIGINT primary key auto_increment,
+    `title`    varchar(64)   NOT NULL,
+    `writer`   varchar(30)   NOT NULL,
+    `contents` text          NOT NULL,
+    `date`     smalldatetime NOT NULL
 );
 
 insert into `user` (`userId`, `name`, `password`, `email`)
@@ -30,13 +30,13 @@ values ('geombong4', '검봉4', 'pwd4', 'email4@123.com');
 insert into `user` (`userId`, `name`, `password`, `email`)
 values ('geombong5', '검봉5', 'pwd5', 'email5@123.com');
 
-insert into `article` (`title`, `writer`, `contents`, `dateOfIssue`)
-values ('JDBC사용법1', 'geombong1', '너무 어렵다1', now());
-insert into `article` (`title`, `writer`, `contents`, `dateOfIssue`)
-values ('JDBC사용법2', 'geombong2', '너무 어렵다2', now() + 1);
-insert into `article` (`title`, `writer`, `contents`, `dateOfIssue`)
-values ('JDBC사용법3', 'geombong3', '너무 어렵다3', now() + 2);
-insert into `article` (`title`, `writer`, `contents`, `dateOfIssue`)
-values ('JDBC사용법4', 'geombong4', '너무 어렵다4', now() + 3);
-insert into `article` (`title`, `writer`, `contents`, `dateOfIssue`)
-values ('JDBC사용법5', 'geombong5', '너무 어렵다5', now() + 4);
+insert into `article` (`title`, `writer`, `contents`, `date`)
+values ('JDBC사용법1', 'geombong1', '너무 어렵다1', now() - 5);
+insert into `article` (`title`, `writer`, `contents`, `date`)
+values ('JDBC사용법2', 'geombong2', '너무 어렵다2', now() - 4);
+insert into `article` (`title`, `writer`, `contents`, `date`)
+values ('JDBC사용법3', 'geombong3', '너무 어렵다3', now() - 3);
+insert into `article` (`title`, `writer`, `contents`, `date`)
+values ('JDBC사용법4', 'geombong4', '너무 어렵다4', now() - 2);
+insert into `article` (`title`, `writer`, `contents`, `date`)
+values ('JDBC사용법5', 'geombong5', '너무 어렵다5', now() - 1);
