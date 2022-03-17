66dcdb74c85fb902239def630bb453165dba4e44
Seung-Wan Jeong <sjeong4776@gmail.com>
2022-03-18 01:03:31 +0900

    Allow only logged-in users to read and write articles
    * LoginRequiredInterceptor가 /questions/** 경로에 대한 요청을 가로채 로그인 상태 여부를 검사한다.

--- src/main/java/kr/codesquad/cafe/system/MvcConfig.java
+++ src/main/java/kr/codesquad/cafe/system/MvcConfig.java
@@ -1,7 +1,10 @@
 package kr.codesquad.cafe.system;
 
+import kr.codesquad.cafe.system.intercepter.LoginRequiredInterceptor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -15,5 +18,16 @@ public class MvcConfig implements WebMvcConfigurer {
 
         registry.addViewController("/join").setViewName("users/form");
         registry.addViewController("/questions/new").setViewName("qna/form");
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginRequiredInterceptor())
+                .addPathPatterns("/questions/**");
+    }
+
+    @Bean
+    public LoginRequiredInterceptor loginRequiredInterceptor() {
+        return new LoginRequiredInterceptor();
     }
 }
