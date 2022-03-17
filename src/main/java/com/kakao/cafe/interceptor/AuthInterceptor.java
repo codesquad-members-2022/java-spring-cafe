61ae2677fcedf3d3ad73bc0938baed2f6ca0f602
naneun <rmtlrksdp@naver.com>
2022-03-18 05:16:50 +0900

    ♻️ AuthInterceptor preHandle, postHandle 메서드에서 권한 확인 후 에러 페이지 이동 방식을 redirect -> forword 으로 변경
    - 예외 페이지로 리다이렉트하는 것이 올바를까요? 계속 고민해보곘습니다.

--- src/main/java/com/kakao/cafe/interceptor/AuthInterceptor.java
+++ src/main/java/com/kakao/cafe/interceptor/AuthInterceptor.java
@@ -5,9 +5,12 @@ import com.kakao.cafe.domain.User;
 import org.apache.logging.log4j.util.Strings;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -22,7 +25,7 @@ public class AuthInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response,
-                             Object handler) throws IOException {
+                             Object handler) throws IOException, ServletException {
 
         debug(request);
 
@@ -41,7 +44,9 @@ public class AuthInterceptor implements HandlerInterceptor {
                 && (Pattern.matches("^/articles/[0-9]+/update$", currentURI)
                 || Pattern.matches("^/articles/[0-9]+/remove$", currentURI))) {
 
-            response.sendRedirect("/error/403");
+            response.setStatus(HttpStatus.UNAUTHORIZED.value());
+            RequestDispatcher requestDispatcher = request.getRequestDispatcher("/error/403");
+            requestDispatcher.forward(request, response);
             return false;
         }
 
@@ -50,7 +55,7 @@ public class AuthInterceptor implements HandlerInterceptor {
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response,
-                           Object handler, ModelAndView modelAndView) throws IOException {
+                           Object handler, ModelAndView modelAndView) throws IOException, ServletException {
 
         String currentURI = request.getRequestURI();
         if (!Pattern.matches("^/articles/[0-9]+/form$", currentURI)) {
@@ -66,7 +71,9 @@ public class AuthInterceptor implements HandlerInterceptor {
         String writer = article.getWriter();
 
         if (Strings.isNotBlank(writer) && !writer.equals(user.getUserId())) {
-            response.sendRedirect("/error/403");
+            response.setStatus(HttpStatus.UNAUTHORIZED.value());
+            RequestDispatcher requestDispatcher = request.getRequestDispatcher("/error/403");
+            requestDispatcher.forward(request, response);
         }
     }
 
