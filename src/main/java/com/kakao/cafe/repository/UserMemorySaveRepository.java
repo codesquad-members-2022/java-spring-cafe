5c3528b08e5975984699e75cc4637ee01835f585
geombong <shoy1415@gmail.com>
2022-03-17 19:02:26 +0900

    feat. 개인정보 수정 기능을 위한 UserUpdate 메서드 추가

--- src/main/java/com/kakao/cafe/repository/UserMemorySaveRepository.java
+++ src/main/java/com/kakao/cafe/repository/UserMemorySaveRepository.java
@@ -2,14 +2,11 @@ package com.kakao.cafe.repository;
 
 import com.kakao.cafe.entity.User;
 
-import org.springframework.stereotype.Repository;
-
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
-@Repository
 public class UserMemorySaveRepository implements UserRepository {
 
     private final List<User> userStore = Collections.synchronizedList(new ArrayList<>());
@@ -39,6 +36,24 @@ public class UserMemorySaveRepository implements UserRepository {
     @Override
     public List<User> findAllUser() {
         return Collections.unmodifiableList(userStore);
+    }
+
+    /**
+     * 앞선 Service 영역에서 findUserId를 통해 null 체크를 하여,
+     * 이 부분에서 orElse() 에 null 이 들어올 일은 없으나,
+     * get 사용으로 인한 경고를 없애기 위해 orElse(null)을 사용
+     *
+     * @param user
+     * @return
+     */
+    @Override
+    public User userUpdate(User user) {
+        int oldUserIndex = userStore
+                .indexOf(
+                        findUserId(user.getUserId()).orElse(null)
+                );
+        userStore.set(oldUserIndex, user);
+        return user;
     }
 
     public void clearStore() {
