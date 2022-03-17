23d84ad9beeccdad8e1ce4a811c14153ac31e0e9
leekm0310 <leekm310@gmail.com>
2022-03-17 20:57:10 +0900

    add: 더미 데이터 추가
    * data.sql에 데이터 추가했습니다.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,11 @@
 INSERT INTO USER (USERID, PASSWORD, NAME, EMAIL) VALUES ('rider37', '1234', 'Heedo', 'nhd@2521.com');
-INSERT INTO USER (USERID, PASSWORD, NAME, EMAIL) VALUES ('injeolme', 'qwer', 'Yoorim', 'kyr@2521.com');
+INSERT INTO USER (USERID, PASSWORD, NAME, EMAIL) VALUES ('injeolmi', 'qwer', 'Yoorim', 'kyr@2521.com');
+INSERT INTO USER (USERID, PASSWORD, NAME, EMAIL) VALUES ('pirate', 'qwer1234', 'Seungwan', 'jsw@2521.com');
+INSERT INTO USER (USERID, PASSWORD, NAME, EMAIL) VALUES ('ipbbuni', '2521', 'Jiwoong', 'mjw@2521.com');
+INSERT INTO USER (USERID, PASSWORD, NAME, EMAIL) VALUES ('beakij', '0123', 'Ijin', 'bij@2521.com');
 
 INSERT INTO ARTICLE (writer, title, contents, created_date) VALUES ('rider37', 'test1', 'hello', '2022-03-16 11:11');
 INSERT INTO ARTICLE (writer, title, contents, created_date) VALUES ('injeolmi', 'test2', 'hello1', '2022-03-16 12:12');
+INSERT INTO ARTICLE (writer, title, contents, created_date) VALUES ('pirate', 'test3', 'test', '2022-03-16 16:12');
+INSERT INTO ARTICLE (writer, title, contents, created_date) VALUES ('ipbbuni', 'test4', 'hihi', '2022-03-16 17:12');
+INSERT INTO ARTICLE (writer, title, contents, created_date) VALUES ('beakij', 'test5', 'nice', '2022-03-16 18:12');
