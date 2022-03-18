977c116441ac6c1c3485702eddd12f792f74d1a9
ikjo93 <auddlr100@naver.com>
2022-03-18 14:55:28 +0900

    fix : 배포, DB 등 환경 설정 파일 누락분 추가

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,22 @@
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user`
+(
+    user_id VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    name VARCHAR(255),
+    email VARCHAR(255),
+    PRIMARY KEY (user_id)
+);
+
+DROP TABLE IF EXISTS article;
+
+CREATE TABLE article
+(
+    id INT NOT NULL AUTO_INCREMENT,
+    writer VARCHAR(255),
+    title VARCHAR(255),
+    contents VARCHAR(255),
+    created_date TIMESTAMP,
+    PRIMARY KEY (id)
+);
