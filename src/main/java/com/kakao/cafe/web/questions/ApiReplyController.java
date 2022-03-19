2416309cfbc55b7c9fda9605eb646df4b6b941f2
jiwoo <zbqmgldjfh@gmail.com>
2022-03-19 10:24:16 +0900

    feat(scripts): Ajax를 통한 댓글 삭제 기능 구현 완료
    삭제 기능 구현이 그냥 form을 이용하자. a 태그를 이용하니까 계속 get방식으로 전달된다. template 자체를 form 으로 만들어 hidden 필드로 delete를 전달하도록 만들었다.

--- src/main/java/com/kakao/cafe/web/questions/ApiReplyController.java
+++ src/main/java/com/kakao/cafe/web/questions/ApiReplyController.java
@@ -1,9 +1,10 @@
-package com.kakao.cafe.web.questions.dto;
+package com.kakao.cafe.web.questions;
 
 import com.kakao.cafe.domain.Reply;
 import com.kakao.cafe.domain.User;
 import com.kakao.cafe.service.ReplyService;
 import com.kakao.cafe.web.SessionConst;
+import com.kakao.cafe.web.questions.dto.Result;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
@@ -25,9 +26,9 @@ public class ApiReplyController {
     }
 
     @DeleteMapping("/{id}")
-    public String deleteReply(@PathVariable Long questionId, @PathVariable Long id, HttpSession session) {
+    public Result deleteReply(@PathVariable Long questionId, @PathVariable Long id, HttpSession session) {
         User sessionedUser = (User) session.getAttribute(SessionConst.SESSIONED_USER);
         replyService.deleteReply(id, sessionedUser); // 댓글 번호와 로그인 사용자 넘기기
-        return "redirect:/questions/" + questionId;
+        return Result.ok();
     }
 }
--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -20,9 +20,40 @@ function addAnswer(e) {
     success: function (data, status) {
       console.log(data);
       var answerTemplate = $("#answerTemplate").html();
-      var template = answerTemplate.format(data.userId, data.localDateTime, data.contents, data.id, data.userId);
+      var template = answerTemplate.format(data.userId, data.localDateTime, data.contents, data.id, data.userId, data.parentArticleId);
       $(".qna-comment-slipp-articles").prepend(template);
       $("textarea[name=contents]").val("");
+      //$(".qna-comment-slipp").load(window.location.href + " .qna-comment-slipp");
+    }
+  });
+}
+
+//$(document).on('click', '.link-delete-article', deleteAnswer)
+//$('.link-delete-article').children('input').click(deleteAnswer);
+$(".qna-comment-slipp-articles").on("click", ".link-delete-article button[type='submit']", deleteAnswer);
+
+function deleteAnswer(e) {
+  e.preventDefault();
+
+  var deleteBtn = $(this);
+  const url = deleteBtn.closest(".link-delete-article").attr("action"); // 값이 왜 바뀌지? var -> const로 변경
+  //var url = $(this).attr("action");
+  console.log("url : " + url);
+
+  $.ajax({
+    type: 'delete',
+    url: url,
+    dataType: 'json',
+    error: function (xhr, status) {
+      console.log("error");
+    },
+    success: function (data, status) {
+      console.log(data);
+      if (data.valid) {
+        deleteBtn.closest("article").remove();
+      } else {
+        alert(data.errorMessage);
+      }
     }
   });
 }
