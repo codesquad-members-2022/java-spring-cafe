deaa2569e58fd8a49546e3d782630f058b6afdaf
dukcode <duk9741@gmail.com>
2022-03-20 16:35:13 +0900

    Test: initial data update

---
+++ src/main/resources/sql/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO userTbl VALUES (1, 'test1', 'test!', '테스트1', 'test1@gmail.com');
+INSERT INTO userTbl VALUES (2, 'test2', 'test!', '테스트2', 'test3@gmail.com');
+INSERT INTO userTbl VALUES (3, 'test3', 'test!', '테스트3', 'test4@gmail.com');
+
+INSERT INTO questionTbl VALUES (1, 'test1', '테스트 게시글1 입니다', '테스트 게시글1 내용입니다');
+INSERT INTO questionTbl VALUES (2, 'test2', '테스트 게시글2 입니다', '테스트 게시글2 내용입니다');
+INSERT INTO questionTbl VALUES (3, 'test3', '테스트 게시글3 입니다', '테스트 게시글3 내용입니다');
