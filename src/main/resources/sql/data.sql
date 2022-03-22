81bb7ec0e423a20c52795167db5d874551e97996
dukcode <duk9741@gmail.com>
2022-03-22 21:29:21 +0900

    Feat: 서버 재시작 시 더미데이터 추가

---
+++ src/main/resources/sql/data.sql
@@ -0,0 +1,10 @@
+insert into userTbl (userId, password, name, email) values ('test1', 'test!', '테스트1', 'test1@gmail.com');
+insert into userTbl (userId, password, name, email) values ('test2', 'test!', '테스트2', 'test2@gmail.com');
+insert into userTbl (userId, password, name, email) values ('test3', 'test3', '테스트3', 'test3@gmail.com');
+
+insert into questionTbl (writer, title, contents) values ('test1', '테스트1의 게시글 제목', '테스트1의 게시글 내용');
+insert into questionTbl (writer, title, contents) values ('test2', '테스트2의 게시글 제목', '테스트2의 게시글 내용');
+insert into questionTbl (writer, title, contents) values ('test3', '테스트3의 게시글 제목', '테스트3의 게시글 내용');
+
+
+
