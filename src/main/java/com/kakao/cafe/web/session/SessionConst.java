67f8278b1c048f4db1998edc74639d3c64b7b63f
dukcode <duk9741@gmail.com>
2022-03-23 21:05:40 +0900

    Refactor: Session 객체에 userId만 저장하도록 변경

--- src/main/java/com/kakao/cafe/web/session/SessionConst.java
+++ src/main/java/com/kakao/cafe/web/session/SessionConst.java
@@ -1,5 +1,5 @@
 package com.kakao.cafe.web.session;
 
 public interface SessionConst {
-    public static String LOGIN_SESSION_NAME = "loginUserSession";
+    public static String LOGIN_SESSION_NAME = "UserSessionUserId";
 }
